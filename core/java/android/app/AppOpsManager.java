5f71efdb17d60d8598927ef478e7c38e30c89575
Sam Mortimer <sam@mortimer.me.uk>
2013-10-20 23:48:00 -0700

    AppOps: track op persistence by name instead of id
    On XML write, include the op name.  On XML read, map
    the name back to op id (if it exists).
    
    Persistent AppOp state now follows the op name instead
    numeric id.  This allows upgrades between versions of
    android that have different code<>name mappings.
    
    Issue: FP2N-175
    Change-Id: Iffc02095b6838a34ff999f8db51c2c6739dd0db8
    (cherry picked from https://github.com/LineageOS/android_frameworks_base/commit/20982c69bb11eaa5f754b3769e2ad841c7f513c1)

--- core/java/android/app/AppOpsManager.java
+++ core/java/android/app/AppOpsManager.java
@@ -964,6 +964,8 @@ public class AppOpsManager {
      */
     private static HashMap<String, Integer> sRuntimePermToOp = new HashMap<>();
 
+    private static HashMap<String, Integer> sNameToOp = new HashMap<String, Integer>();
+
     static {
         if (sOpToSwitch.length != _NUM_OP) {
             throw new IllegalStateException("sOpToSwitch length " + sOpToSwitch.length
@@ -1007,6 +1009,9 @@ public class AppOpsManager {
                 sRuntimePermToOp.put(sOpPerms[op], op);
             }
         }
+        for (int i=0; i<_NUM_OP; i++) {
+            sNameToOp.put(sOpNames[i], i);
+        }
     }
 
     /**
@@ -1036,6 +1041,15 @@ public class AppOpsManager {
             }
         }
         throw new IllegalArgumentException("Unknown operation string: " + op);
+    }
+
+    /**
+     * Map a non-localized name for the operation back to the Op number
+     * @hide
+     */
+    public static int nameToOp(String name) {
+        Integer val = sNameToOp.get(name);
+        return val != null ? val : OP_NONE;
     }
 
     /**
