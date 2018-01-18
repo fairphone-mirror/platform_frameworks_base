96143d82334c40b7d3dd122ae35e2acc827e0f08
Borjan Tchakaloff <borjan@fairphone.com>
2018-01-18 12:56:41 +0100

    Use the battery design capacity exposed by the kernel
    Add support for battery packs whose design capacity is exposed by the
    kernel. Needed if multiple battery packs with different capacities are
    available for the platform as the power profile is statically set with
    a fixed value.
    
    Changes in the cherry-pick: Adjust to new data structures in Android 9
    (sPowerItemMap instead of sPowerMap).
    
    Cherry-pick to FP3 Android 13: Read value from exposed system property
    instead, since sysfs is not accessible in (third party) app context.
    
    Issue: FPIIM-2410
    Issue: FP2P-109
    Issue: FP2P-264
    Issue: FP2A10-16
    Issue: FP3-A13#86
    Change-Id: Id93d13fb8de63a2375f2948449a380ebb746a166
    (cherry picked from commit efaa929a0a41c86d866c353042df5d98c73bbbe2)
    (cherry picked from commit b1ae55af9bd7cdc54b3b805bfa57dfaab1ba7dac)
    Depends-On: Ia025ddf9621a6012cd8b0a876218183705d0e886
    Depends-On: I4770a690281f327f85de301e26223c3fe0b87c28

--- core/java/com/android/internal/os/PowerProfile.java
+++ core/java/com/android/internal/os/PowerProfile.java
@@ -27,6 +27,7 @@ import android.content.res.XmlResourceParser;
 import android.util.IndentingPrintWriter;
 import android.util.Slog;
 import android.util.proto.ProtoOutputStream;
+import android.os.SystemProperties;
 
 import com.android.internal.annotations.GuardedBy;
 import com.android.internal.annotations.VisibleForTesting;
@@ -313,6 +314,9 @@ public class PowerProfile {
 
     private static final String TAG_MODEM = "modem";
 
+    private static final String PROPERTY_BATTERY_DESIGN_CAPACITY =
+            "ro.hardware.battery_design_capacity";
+
     private static final Object sLock = new Object();
 
     @VisibleForTesting
@@ -442,6 +446,18 @@ public class PowerProfile {
                 sPowerItemMap.put(key, (double) value);
             }
         }
+
+        /*
+         * There is no way to set the battery design capacity automatically here from what the
+         * kernel exposes in the sysfs. Several battery packs can be available for the device and
+         * they might have different capacities. So do not rely on the XML power profile and always
+         * use what the kernel exposes instead.
+         */
+        // Read the battery design capacity from system property, in microampere-hour
+        final int batteryDesignCapacityMicro = SystemProperties.getInt(
+            PROPERTY_BATTERY_DESIGN_CAPACITY, 0);
+        // capacity in power profile is in milliampere-hour
+        sPowerItemMap.put(POWER_BATTERY_CAPACITY, ((double) batteryDesignCapacityMicro) / 1000);
     }
 
     private CpuClusterKey[] mCpuClusters;
