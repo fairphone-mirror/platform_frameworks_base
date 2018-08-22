18289595eebd01a38a771866ebbbaaaba4b06ddf
Andrew Appleby <andrewa@fairphone.com>
2018-08-22 11:05:06 +0200

    Revert "appops: Add support for SU"
    This reverts commit 972321ffedc5f535e27dca878ab6bbba4b31cf10.
    
    The SuperUser feature crashes the phone on initialization. Let's
    remove it for now.
    
    Issue: FP2N-278
    Change-Id: I601eedc12301696e9ee69c3b3306313b067d48d1

--- core/java/android/app/AppOpsManager.java
+++ core/java/android/app/AppOpsManager.java
@@ -257,9 +257,7 @@ public class AppOpsManager {
     /** @hide BLUETOOTH_ADMIN . */
     public static final int OP_BLUETOOTH_ADMIN = 65;
     /** @hide */
-    public static final int OP_SU = 66;
-    /** @hide */
-    public static final int _NUM_OP = 67;
+    public static final int _NUM_OP = 66;
 
     /** Access to coarse location information. */
     public static final String OPSTR_COARSE_LOCATION = "android:coarse_location";
@@ -366,8 +364,6 @@ public class AppOpsManager {
     /** @hide PROCESS_OUTGOING_CALLS. */
     public static final String OPSTR_PROCESS_OUTGOING_CALLS
             = "android:process_outgoing_calls";
-    private static final String OPSTR_SU =
-            "android:su";
 
     private static final int[] RUNTIME_PERMISSIONS_OPS = {
             // Contacts
@@ -481,7 +477,6 @@ public class AppOpsManager {
             OP_RUN_IN_BACKGROUND,
             OP_CHANGE_WIFI_STATE,
             OP_BLUETOOTH_ADMIN,
-            OP_SU
     };
 
     /**
@@ -555,7 +550,6 @@ public class AppOpsManager {
             null,
             OPSTR_CHANGE_WIFI_STATE,
             OPSTR_BLUETOOTH_ADMIN,
-            OPSTR_SU,
     };
 
     /**
@@ -629,7 +623,6 @@ public class AppOpsManager {
             "RUN_IN_BACKGROUND",
             "CHANGE_WIFI_STATE",
             "BLUETOOTH_ADMIN",
-            "SU",
     };
 
     /**
@@ -703,7 +696,6 @@ public class AppOpsManager {
             null, // no permission for running in background
             android.Manifest.permission.CHANGE_WIFI_STATE,
             android.Manifest.permission.BLUETOOTH_ADMIN,
-            null,
     };
 
     /**
@@ -778,7 +770,6 @@ public class AppOpsManager {
             null, // RUN_IN_BACKGROUND
             null, // OP_CHANGE_WIFI_STATE
             null, // OP_BLUETOOTH_ADMIN
-            UserManager.DISALLOW_SU, //SU TODO: this should really be investigated.
     };
 
     /**
@@ -852,7 +843,6 @@ public class AppOpsManager {
             false, // RUN_IN_BACKGROUND
             false, // OP_CHANGE_WIFI_STATE
             false, // OP_BLUETOOTH_ADMIN
-            false, //SU
     };
 
     /**
@@ -925,7 +915,6 @@ public class AppOpsManager {
             AppOpsManager.MODE_ALLOWED,  // OP_RUN_IN_BACKGROUND
             AppOpsManager.MODE_ALLOWED,  //OP_CHANGE_WIFI_STATE
             AppOpsManager.MODE_ALLOWED,  //OP_BLUETOOTH_ADMIN
-            AppOpsManager.MODE_ASK, // OP_SU
     };
 
     /**
@@ -999,7 +988,6 @@ public class AppOpsManager {
             AppOpsManager.MODE_ALLOWED, // MODE_RUN_IN_BACKGROUND
             AppOpsManager.MODE_ASK,     // OP_CHANGE_WIFI_STATE
             AppOpsManager.MODE_ASK,     // OP_BLUETOOTH_CHANGE
-            AppOpsManager.MODE_ASK,     // OP_SU
     };
 
     /**
@@ -1072,7 +1060,6 @@ public class AppOpsManager {
         false,    // RUN_IN_BACKGROUND
         true,     // OP_CHANGE_WIFI_STATE
         true,     // OP_BLUETOOTH_CHANGE
-        true,     // OP_SU
     };
 
     /**
@@ -1149,7 +1136,6 @@ public class AppOpsManager {
             false,
             false, //OP_CHANGE_WIFI_STATE
             false, //OP_BLUETOOTH_ADMIN
-            false,     // OP_SU
     };
 
     /**
