5a70fe0c016bd08bb703837890f178c4f87d1364
yidechen <yidechen@arimacomm.com.tw>
2019-07-03 13:40:31 +0800

    Move volume control to left side of screen
    Issue: FP3-A11#100
    Issue: FP3-A11#183
    Issue: FP3-A11#233
    Change-Id: Ib8847776d5ce51718cde14336062a6bbff5c8a79
    (cherry picked from commit 6e6cea46d4d163bc55978bff89ab68257459f9f3)
    (cherry picked from commit d3e6267856584bba13943218c11e184892509ff4)

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -430,7 +430,7 @@ public class VolumeDialogImpl implements VolumeDialog,
         mDialog.setOnShowListener(dialog -> {
             mDialogView.getViewTreeObserver().addOnComputeInternalInsetsListener(this);
             if (!shouldSlideInVolumeTray()) {
-                mDialogView.setTranslationX(mDialogView.getWidth() / 2.0f);
+                mDialogView.setTranslationX(-mDialogView.getWidth() / 2.0f);
             }
             mDialogView.setAlpha(0);
             mDialogView.animate()
@@ -1381,7 +1381,7 @@ public class VolumeDialogImpl implements VolumeDialog,
 
                     hideRingerDrawer();
                 }, 50));
-        if (!shouldSlideInVolumeTray()) animator.translationX(mDialogView.getWidth() / 2.0f);
+        if (!shouldSlideInVolumeTray()) animator.translationX(-mDialogView.getWidth() / 2.0f);
         animator.setListener(getJankListener(getDialogView(), TYPE_DISMISS,
                 mDialogHideAnimationDurationMs)).start();
         checkODICaptionsTooltip(true);
