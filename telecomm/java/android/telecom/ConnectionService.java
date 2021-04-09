1878093da95c232758fb8770804a202e11054615
Muhammed Siju <msiju@codeaurora.org>
2021-04-09 19:52:32 +0530

    IMS: Fix issue with answering call for some 3rd party apps
    Some 3rd party call manager apps, implement only onAnswer()
    method for answering calls. So make sure onAnswer() is invoked
    when answer call is requested without videostate.
    
    Issue: FP3-A13#56
    CRs-Fixed: 2921144
    Change-Id: I033279e4bd17fa291ca2cb8f149e33264446d24a
    (cherry picked from commit 3d24e82f45a62c27c1f27228aced6e2cb9afd49e)

--- telecomm/java/android/telecom/ConnectionService.java
+++ telecomm/java/android/telecom/ConnectionService.java
@@ -2216,7 +2216,8 @@ public abstract class ConnectionService extends Service {
         findConnectionForAction(callId, "abort").onAbort();
     }
 
-    private void answerVideo(String callId, int videoState) {
+    /** {@hide} */
+    protected void answerVideo(String callId, int videoState) {
         Log.i(this, "answerVideo %s", callId);
         if (mConnectionById.containsKey(callId)) {
             findConnectionForAction(callId, "answer").onAnswer(videoState);
@@ -2225,7 +2226,8 @@ public abstract class ConnectionService extends Service {
         }
     }
 
-    private void answer(String callId) {
+    /** {@hide} */
+    protected void answer(String callId) {
         Log.i(this, "answer %s", callId);
         if (mConnectionById.containsKey(callId)) {
             findConnectionForAction(callId, "answer").onAnswer();
