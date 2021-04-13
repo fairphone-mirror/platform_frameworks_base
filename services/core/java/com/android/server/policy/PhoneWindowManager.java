eaada984124b9ec28445e6a8d61b23a3b39774f7
chuanzhi.shao <chuanzhi.shao@t2mobile.com>
2021-04-13 18:29:14 +0800

    alm 11006885 powerkey return to user
    Change-Id: I47245341e05e76cd6ecdd04c1f6a511f40272c63

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -2669,6 +2669,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         // it handle it, because that gives us the correct 5 second
         // timeout.
         if (keyCode == KeyEvent.KEYCODE_HOME) {
+            if(mDefaultDisplayPolicy.blockKeysForMiniTest(false)) {
+                Log.i(TAG, "home key pressed in mmitest.");
+                return 0;
+            }
             DisplayHomeButtonHandler handler = mDisplayHomeButtonHandlers.get(displayId);
             if (handler == null) {
                 handler = new DisplayHomeButtonHandler(displayId);
@@ -2676,6 +2680,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             }
             return handler.handleHomeButton(focusedToken, event);
         } else if (keyCode == KeyEvent.KEYCODE_MENU) {
+            if(SystemProperties.getBoolean("dev.tct.MMITestPower", false)) {
+                Log.d(TAG, "menu key pressed in mmitest");
+                return 0;
+            }
             // Hijack modified menu keys for debugging features
             final int chordBug = KeyEvent.META_SHIFT_ON;
 
@@ -2702,6 +2710,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             }
             return 0;
         } else if (keyCode == KeyEvent.KEYCODE_APP_SWITCH) {
+            if(SystemProperties.getBoolean("dev.tct.MMITestPower", false)) {
+                Log.d(TAG, "app switch key pressed in mmitest");
+                return 0;
+            }
             if (!keyguardOn) {
                 if (down && repeatCount == 0) {
                     preloadRecentApps();
@@ -3852,6 +3864,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             }
 
             case KeyEvent.KEYCODE_POWER: {
+                if(SystemProperties.getBoolean("dev.tct.MMITestPower", false)) {
+                    Log.d(TAG, "power key pressed in mmitest");
+                    return ACTION_PASS_TO_USER;
+                }
                 EventLogTags.writeInterceptPower(
                         KeyEvent.actionToString(event.getAction()),
                         mPowerKeyHandled ? 1 : 0, mPowerKeyPressCounter);
--- services/core/java/com/android/server/wm/DisplayPolicy.java
+++ services/core/java/com/android/server/wm/DisplayPolicy.java
@@ -216,6 +216,12 @@ import com.android.server.wm.utils.InsetUtils;
 import java.io.PrintWriter;
 import java.util.function.Consumer;
 
+import android.app.ActivityManager.RunningTaskInfo;
+import android.content.ComponentName;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * The policy that provides the basic behaviors and states of a display to show UI.
  */
@@ -579,7 +585,9 @@ public class DisplayPolicy {
                     public void onSwipeFromTop() {
                         synchronized (mLock) {
                             if (mStatusBar != null) {
-                                requestTransientBars(mStatusBar);
+                                if(!isMMITestTop(null)) {
+                                    requestTransientBars(mStatusBar);
+                                }
                             }
                             checkAltBarSwipeForTransientBars(ALT_BAR_TOP);
                         }
@@ -590,7 +598,9 @@ public class DisplayPolicy {
                         synchronized (mLock) {
                             if (mNavigationBar != null
                                     && mNavigationBarPosition == NAV_BAR_BOTTOM) {
-                                requestTransientBars(mNavigationBar);
+                                if(!isMMITestTop(null)) {
+                                    requestTransientBars(mNavigationBar);
+                                }
                             }
                             checkAltBarSwipeForTransientBars(ALT_BAR_BOTTOM);
                         }
@@ -4411,4 +4421,55 @@ public class DisplayPolicy {
 
         return Rect.intersects(targetWindow.getFrameLw(), navBarWindow.getFrameLw());
     }
+
+    private static final String[] sMMITestPkgs = {
+              "com.android.mmi",
+              "com.android.autommi",
+              "com.gim.mmi"
+      };
+  
+      private static final List<String> sMMITestPkgList = new ArrayList<String>(
+              Arrays.asList(sMMITestPkgs));
+  
+      private boolean isMMITestTop(WindowState win) {
+          final WindowState curWin = mFocusedWindow != null ? mFocusedWindow
+                  : mTopFullscreenOpaqueWindowState;
+          WindowState windowState = null;
+          if (win != null && win.getAttrs() != null) {
+              windowState = win;
+          } else if (curWin != null && curWin.getAttrs() != null) {
+              windowState = curWin;
+          }
+  
+          if (windowState != null && windowState.getAttrs() != null) {
+              final String pkgname = windowState.getAttrs().packageName;
+              if (pkgname != null && sMMITestPkgList.contains(pkgname)) {
+                  Slog.d(TAG, "isMMITestTop = true");
+                  return true;
+              }
+          }
+          Slog.d(TAG, "isMMITestTop = false");
+          return false;
+      }
+  
+      public boolean blockKeysForMiniTest(boolean isSpecificTest) {
+          ActivityManager am = (ActivityManager) mContext.getSystemService(Context.ACTIVITY_SERVICE);
+          List<RunningTaskInfo> tasks = am.getRunningTasks(1);
+          ComponentName cn = null;
+          final WindowState curWin = (mFocusedWindow != null) ? mFocusedWindow : mTopFullscreenOpaqueWindowState;
+  
+          if (tasks.size() > 0)
+              cn = tasks.get(0).topActivity;
+  
+          if (cn != null) {
+              final String pkgname = cn.getPackageName();
+              if (pkgname != null && sMMITestPkgList.contains(pkgname)) {
+                  Slog.d(TAG, "isMMITestTop = true");
+                  return true;
+              }
+          }
+          Slog.d(TAG, "isMMITestTop = false");
+          return false;
+
+      }
 }
