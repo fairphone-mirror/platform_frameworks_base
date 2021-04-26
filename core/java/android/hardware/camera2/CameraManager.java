d9edc7486e3524d139007203b16ecbee601465bd
junwen.ye <junwen.ye@t2mobile.com>
2021-04-26 11:31:43 +0800

    [ALM:10872982] [FP4]:expose aux to mmi
     &&&%%%comment:[FP4]:expose aux to mmi
     &&&%%%bug number:10872982
     &&&%%%product name:sm7225_r_fp4
     &&&%%%root cause:coding
     &&&%%%Bug category:T2M
     &&&%%%Module_Impact:framework
     &&&%%%Test_Suggestion:NA
     &&&%%%Solution:NA
     &&&%%%Test_Report:ok in native
     &&&%%%VAL Can Test:NA

--- core/java/android/hardware/camera2/CameraManager.java
+++ core/java/android/hardware/camera2/CameraManager.java
@@ -1653,14 +1653,19 @@ public final class CameraManager {
             String[] cameraIds = null;
             boolean exposeAuxCamera = false;
             String packageName = ActivityThread.currentOpPackageName();
-            String packageList = SystemProperties.get("vendor.camera.aux.packagelist");
-            if (packageList.length() > 0) {
-                TextUtils.StringSplitter splitter = new TextUtils.SimpleStringSplitter(',');
-                splitter.setString(packageList);
-                for (String str : splitter) {
-                    if (packageName.equals(str)) {
-                        exposeAuxCamera = true;
-                        break;
+            if (packageName.contains("com.android.mmi")) {
+                  exposeAuxCamera = true;
+            } else {
+                String packageList = SystemProperties.get("vendor.camera.aux.packagelist",
+                       "org.codeaurora.snapcam,com.android.mmi");
+                if (packageList.length() > 0) {
+                    TextUtils.StringSplitter splitter = new TextUtils.SimpleStringSplitter(',');
+                    splitter.setString(packageList);
+                    for (String str : splitter) {
+                        if (packageName.equals(str)) {
+                            exposeAuxCamera = true;
+                            break;
+                        }
                     }
                 }
             }
@@ -1945,14 +1950,19 @@ public final class CameraManager {
                  */
                 boolean exposeAuxCamera = false;
                 String packageName = ActivityThread.currentOpPackageName();
-                String packageList = SystemProperties.get("vendor.camera.aux.packagelist");
-                if (packageList.length() > 0) {
-                    TextUtils.StringSplitter splitter = new TextUtils.SimpleStringSplitter(',');
-                    splitter.setString(packageList);
-                    for (String str : splitter) {
-                        if (packageName.equals(str)) {
-                            exposeAuxCamera = true;
-                            break;
+                if (packageName.contains("com.android.mmi")) {
+                      exposeAuxCamera = true;
+                } else {
+                    String packageList = SystemProperties.get("vendor.camera.aux.packagelist",
+                           "org.codeaurora.snapcam,com.android.mmi");
+                    if (packageList.length() > 0) {
+                        TextUtils.StringSplitter splitter = new TextUtils.SimpleStringSplitter(',');
+                        splitter.setString(packageList);
+                        for (String str : splitter) {
+                            if (packageName.equals(str)) {
+                                exposeAuxCamera = true;
+                                break;
+                            }
                         }
                     }
                 }
@@ -2232,14 +2242,19 @@ public final class CameraManager {
              */
             boolean exposeMonoCamera = false;
             String packageName = ActivityThread.currentOpPackageName();
-            String packageList = SystemProperties.get("vendor.camera.aux.packagelist");
-            if (packageList.length() > 0) {
-                TextUtils.StringSplitter splitter = new TextUtils.SimpleStringSplitter(',');
-                splitter.setString(packageList);
-                for (String str : splitter) {
-                    if (packageName.equals(str)) {
-                        exposeMonoCamera = true;
-                        break;
+            if (packageName.contains("com.android.mmi")) {
+                  exposeMonoCamera = true;
+            } else {
+                String packageList = SystemProperties.get("vendor.camera.aux.packagelist",
+                       "org.codeaurora.snapcam,com.android.mmi");
+                if (packageList.length() > 0) {
+                    TextUtils.StringSplitter splitter = new TextUtils.SimpleStringSplitter(',');
+                    splitter.setString(packageList);
+                    for (String str : splitter) {
+                        if (packageName.equals(str)) {
+                            exposeMonoCamera = true;
+                            break;
+                        }
                     }
                 }
             }
@@ -2387,14 +2402,19 @@ public final class CameraManager {
              */
             boolean exposeMonoCamera = false;
             String packageName = ActivityThread.currentOpPackageName();
-            String packageList = SystemProperties.get("vendor.camera.aux.packagelist");
-            if (packageList.length() > 0) {
-                TextUtils.StringSplitter splitter = new TextUtils.SimpleStringSplitter(',');
-                splitter.setString(packageList);
-                for (String str : splitter) {
-                    if (packageName.equals(str)) {
-                        exposeMonoCamera = true;
-                        break;
+            if (packageName.contains("com.android.mmi")) {
+                  exposeMonoCamera = true;
+            } else {
+                String packageList = SystemProperties.get("vendor.camera.aux.packagelist",
+                       "org.codeaurora.snapcam,com.android.mmi");
+                if (packageList.length() > 0) {
+                    TextUtils.StringSplitter splitter = new TextUtils.SimpleStringSplitter(',');
+                    splitter.setString(packageList);
+                    for (String str : splitter) {
+                        if (packageName.equals(str)) {
+                            exposeMonoCamera = true;
+                            break;
+                        }
                     }
                 }
             }
