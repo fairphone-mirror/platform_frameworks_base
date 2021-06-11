09dec064e2701355599b5b88761d04e6d19f1790
xiaoyang.wang <xiaoyang.wang@t2mobile.com>
2021-06-11 13:13:47 +0800

    FP4-835 [SW REQ]NFC icon display error(在statusbar上添加NFC图标)
    Change-Id: I68d97d4acfe8c44745073f9556328223cd941d0f

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
@@ -44,6 +44,8 @@ import android.telecom.TelecomManager;
 import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.View;
+import android.nfc.NfcAdapter;
+
 
 import androidx.lifecycle.Observer;
 
@@ -124,6 +126,7 @@ public class PhoneStatusBarPolicy
     private final String mSlotCamera;
     private final String mSlotSensorsOff;
     private final String mSlotScreenRecord;
+    private final String mSlotNfc;
     private final int mDisplayId;
     private final SharedPreferences mSharedPreferences;
     private final DateFormatUtil mDateFormatUtil;
@@ -230,7 +233,7 @@ public class PhoneStatusBarPolicy
         mSlotSensorsOff = resources.getString(com.android.internal.R.string.status_bar_sensors_off);
         mSlotScreenRecord = resources.getString(
                 com.android.internal.R.string.status_bar_screen_record);
-
+        mSlotNfc = resources.getString(com.android.internal.R.string.status_bar_nfc);
         mDisplayId = displayId;
         mSharedPreferences = sharedPreferences;
         mDateFormatUtil = dateFormatUtil;
@@ -247,6 +250,7 @@ public class PhoneStatusBarPolicy
         filter.addAction(Intent.ACTION_MANAGED_PROFILE_AVAILABLE);
         filter.addAction(Intent.ACTION_MANAGED_PROFILE_UNAVAILABLE);
         filter.addAction(Intent.ACTION_MANAGED_PROFILE_REMOVED);
+        filter.addAction(NfcAdapter.ACTION_ADAPTER_STATE_CHANGED);
         mBroadcastDispatcher.registerReceiverWithHandler(mIntentReceiver, filter, mHandler);
         Observer<Integer> observer = ringer -> mHandler.post(this::updateVolumeZen);
 
@@ -740,6 +744,9 @@ public class PhoneStatusBarPolicy
                 case AudioManager.ACTION_HEADSET_PLUG:
                     updateHeadsetPlug(intent);
                     break;
+                case NfcAdapter.ACTION_ADAPTER_STATE_CHANGED:
+                    updateNfc(intent);
+                    break;
             }
         }
     };
@@ -815,6 +822,25 @@ public class PhoneStatusBarPolicy
         } else {
             mIconController.setIcon(mSlotHotspot, R.drawable.stat_sys_hotspot,
                 mResources.getString(R.string.accessibility_status_bar_hotspot));
+        }
+    }
+
+    private final void updateNfc(Intent intent) {
+        final int state = intent.getIntExtra(NfcAdapter.EXTRA_ADAPTER_STATE, NfcAdapter.STATE_OFF);
+        Log.v(TAG, "Nfc state = "+ state);
+        switch (state) {
+            case NfcAdapter.STATE_OFF:
+                mIconController.setIconVisibility(mSlotNfc, false);
+                break;
+            case NfcAdapter.STATE_TURNING_OFF:
+                break;
+            case NfcAdapter.STATE_ON:
+                mIconController.setIcon(mSlotNfc, R.drawable.ic_qs_nfc_enabled,
+                    mResources.getString(R.string.quick_settings_nfc_on));  
+                mIconController.setIconVisibility(mSlotNfc, true);
+                break;
+            case NfcAdapter.STATE_TURNING_ON:
+                break;
         }
     }
 }
