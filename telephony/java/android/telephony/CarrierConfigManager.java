0d0f52369c4a727f2d98f2b0c797cdc28b2376af
minjie.cai <minjie.cai@t2mobile.com>
2021-06-24 21:21:00 +0800

    [FP4-654] [FP4]:GtsNmgiarcTestCases:2 cases failed
     &&&%%%comment:[FP4]:GtsNmgiarcTestCases:2 cases failed
     &&&%%%bug number:FP4-654
     &&&%%%product name:sm7225_r_fp4
     &&&%%%root cause:coding
     &&&%%%Bug category:T2M
     &&&%%%Module_Impact:framework
     &&&%%%Test_Suggestion:normal test
     &&&%%%Solution:coding
     &&&%%%Test_Report:test is ok
     &&&%%%VAL Can Test:NO

--- telephony/java/android/telephony/CarrierConfigManager.java
+++ telephony/java/android/telephony/CarrierConfigManager.java
@@ -5469,7 +5469,7 @@ public class CarrierConfigManager {
                 });
         sDefaults.putStringArray(KEY_GSM_ROAMING_NETWORKS_STRING_ARRAY, null);
         sDefaults.putStringArray(KEY_GSM_NONROAMING_NETWORKS_STRING_ARRAY, null);
-        sDefaults.putString(KEY_CONFIG_IMS_PACKAGE_OVERRIDE_STRING, null);
+        sDefaults.putString(KEY_CONFIG_IMS_PACKAGE_OVERRIDE_STRING, "com.google.android.ims");
         sDefaults.putString(KEY_CONFIG_IMS_MMTEL_PACKAGE_OVERRIDE_STRING, null);
         sDefaults.putString(KEY_CONFIG_IMS_RCS_PACKAGE_OVERRIDE_STRING, null);
         sDefaults.putStringArray(KEY_CDMA_ROAMING_NETWORKS_STRING_ARRAY, null);
