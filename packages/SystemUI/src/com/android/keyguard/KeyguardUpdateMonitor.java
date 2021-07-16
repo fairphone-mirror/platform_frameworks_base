b49cdf4eceba7ec702dfc849fc3cda0b9b599ce2
minjie.cai <minjie.cai@t2mobile.com>
2021-07-16 11:19:31 +0800

    [FP4-1206] [FP4]:There is no NXTVISION app in the settings after flash the new version or factory reset
     &&&%%%comment:[FP4]:There is no NXTVISION app in the settings after flash the new version or factory reset
     &&&%%%bug number:FP4-1206
     &&&%%%product name:sm7225_r_fp4
     &&&%%%root cause:coding
     &&&%%%Bug category:T2M
     &&&%%%Module_Impact:settings
     &&&%%%Test_Suggestion:normal test
     &&&%%%Solution:coding
     &&&%%%Test_Report:test is ok
     &&&%%%VAL Can Test:NO

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -2643,6 +2643,9 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
             // We don't need the observer anymore...
             mContext.getContentResolver().unregisterContentObserver(mDeviceProvisionedObserver);
             mDeviceProvisionedObserver = null;
+
+            ActivityManager am = (ActivityManager) mContext.getSystemService(Context.ACTIVITY_SERVICE);
+            am.restartPackage("com.android.settings");
         }
     }
 
