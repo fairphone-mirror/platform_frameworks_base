ea22f7e45c6d0103f5a4b084c0357c407e220a4a
zhangtianwen <tianwen.zhang@t2mobile.com>
2021-08-18 17:47:40 +0800

    Uploaded image for project: 'P_T2M-FP4' P_T2M-FP4FP4-2446 [Orange]03. In-app activities-03_05_InstallOneApp_from_IS
    Change-Id: I7eb52686cbe12daaf9d8a58c974c60a61677e3e4

--- services/core/java/com/android/server/pm/AppStateController.java
+++ services/core/java/com/android/server/pm/AppStateController.java
@@ -14,12 +14,15 @@ import android.content.pm.IPackageManager;
 import android.content.IntentFilter;
 import android.content.Intent;
 import android.content.BroadcastReceiver;
+import android.os.SystemProperties;
 import android.telephony.CarrierConfigManager;
 import android.telephony.SubscriptionManager;
 import android.telephony.SubscriptionInfo;
 import android.telephony.TelephonyManager;
+import android.text.TextUtils;
 import android.util.Log;
 import android.os.Process;
+import android.provider.Settings;
 
 import static android.os.UserHandle.USER_SYSTEM;
 
@@ -34,15 +37,25 @@ import com.android.internal.telephony.PhoneConstants;
 public class AppStateController {
 
     private final String TAG = "AppStateController";
+
+    private static final String SETTINGS_CONFIG_SIM_LOCK_GID = "sim_lock_gid";
+
+    private static final String SETTINGS_CONFIG_SIM_LOCK_SPN = "sim_lock_spn";
+
+    private static final String SETTINGS_CONFIG_SIM_LOCK_MCCMNC = "sim_lock_mccmnc";
+
     private Context mContext;
     private Handler mHandler;
     SubscriptionManager mSubscriptionManager;
 
     private String spn0, plmn0, iccid0;
     private String spn1, plmn1, iccid1;
+    private String gid1_0, gid1_1;
+
+    private MyCarrierInfo mMyCarrierInfo;
+
     private int simCount = 0;
 
-    private int mUserId;
 
     /**
      * @param context
@@ -52,7 +65,6 @@ public class AppStateController {
     public AppStateController(Context context, Handler handler) {
         this.mContext = context;
         this.mHandler = handler;
-        mUserId = Process.myUserHandle().myUserId();
         registerReceiver();
     }
 
@@ -139,35 +151,35 @@ public class AppStateController {
     //ATT defect 8743303 10658538
     private final String[] attApps = {"com.att.miatt"};
     //[TCT-ROM]Begin modify by tairan.hao for 10691772 on 2021-02-04
-    private final String[] attSIMPlmn = {"33405","334050","334090","33470","33450"};
+    private final String[] attSIMPlmn = {"33405", "334050", "334090", "33470", "33450"};
     //[TCT-ROM]End modify by tairan.hao for 10691772 on 2021-02-04
-    AppUnderControll att = new AppUnderControll(attApps){
+    AppUnderControll att = new AppUnderControll(attApps) {
         @Override
-        public boolean isSimAppropriate(){
-            if(iccid0 != null && iccid0.length() >= 9){
-                String sub = iccid0.substring(8,9);
-                Log.d(TAG,"iccid0[8]= " + sub);
+        public boolean isSimAppropriate() {
+            if (iccid0 != null && iccid0.length() >= 9) {
+                String sub = iccid0.substring(8, 9);
+                Log.d(TAG, "iccid0[8]= " + sub);
                 //[TCT-ROM]Begin add by tairan.hao for 10658538 on 2021-01-18
-                if("0".equals(sub) || "2".equals(sub) ||
+                if ("0".equals(sub) || "2".equals(sub) ||
                         "4".equals(sub) || "5".equals(sub) ||
                         "6".equals(sub) || "7".equals(sub) ||
-                        "8".equals(sub) || "9".equals(sub)){
-                //[TCT-ROM]End add by tairan.hao for 10658538 on 2021-01-18
-                    if(isSubInfoAppropriate(attSIMPlmn, plmn0)){
+                        "8".equals(sub) || "9".equals(sub)) {
+                    //[TCT-ROM]End add by tairan.hao for 10658538 on 2021-01-18
+                    if (isSubInfoAppropriate(attSIMPlmn, plmn0)) {
                         return true;
                     }
                 }
             }
-            if(iccid1 != null && iccid1.length() >= 9){
-                String sub =  iccid1.substring(8,9);
-                Log.d(TAG,"iccid1[8] =" + sub);
+            if (iccid1 != null && iccid1.length() >= 9) {
+                String sub = iccid1.substring(8, 9);
+                Log.d(TAG, "iccid1[8] =" + sub);
                 //[TCT-ROM]Begin add by tairan.hao for 10658538 on 2021-01-18
-                if("0".equals(sub) || "2".equals(sub) ||
+                if ("0".equals(sub) || "2".equals(sub) ||
                         "4".equals(sub) || "5".equals(sub) ||
                         "6".equals(sub) || "7".equals(sub) ||
-                        "8".equals(sub) || "9".equals(sub)){
-                //[TCT-ROM]End add by tairan.hao for 10658538 on 2021-01-18
-                    if(isSubInfoAppropriate(attSIMPlmn, plmn1)){
+                        "8".equals(sub) || "9".equals(sub)) {
+                    //[TCT-ROM]End add by tairan.hao for 10658538 on 2021-01-18
+                    if (isSubInfoAppropriate(attSIMPlmn, plmn1)) {
                         return true;
                     }
                 }
@@ -217,20 +229,20 @@ public class AppStateController {
     //TWO_DEGREES Task 9463498
     private final String[] TWO_DEGREES_APPS = {"com.twodegreesmobile.twodegrees"};
     private final String[] twoDegreesSIMPlmn = {"53024"};
-    AppUnderControll twoDegrees = new AppUnderControll(TWO_DEGREES_APPS){
+    AppUnderControll twoDegrees = new AppUnderControll(TWO_DEGREES_APPS) {
         @Override
-        public void setNewState(){
+        public void setNewState() {
             //if not first boot, don't disable 2Degrees apps
-            if(isFirstBoot() && !isSimAppropriate()){
+            if (isFirstBoot() && !isSimAppropriate()) {
                 twoDegrees.newState = PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
             }
-            if(isSimAppropriate()){
+            if (isSimAppropriate()) {
                 twoDegrees.newState = PackageManager.COMPONENT_ENABLED_STATE_DEFAULT;
             }
         }
 
         @Override
-        public boolean isSimAppropriate(){
+        public boolean isSimAppropriate() {
             return "2degrees".equals(spn0) && isSubInfoAppropriate(twoDegreesSIMPlmn, plmn0) ||
                     "2degrees".equals(spn1) && isSubInfoAppropriate(twoDegreesSIMPlmn, plmn1);
         }
@@ -258,7 +270,7 @@ public class AppStateController {
             if (isFirstBoot() && !isSimAppropriate()) {
                 dt.newState = PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
             }
-            if(isSimAppropriate()){
+            if (isSimAppropriate()) {
                 dt.newState = PackageManager.COMPONENT_ENABLED_STATE_DEFAULT;
             }
         }
@@ -316,15 +328,15 @@ public class AppStateController {
     private void updateAppState() {
         if (mHandler == null) return;
         getSubscriptionInfo(mContext);
-        mHandler.post(()->
-            //updateAppState(orange,att,unefon,twoDegrees)
-                updateAppState(orange,dt)
+        mHandler.post(() ->
+                //updateAppState(orange,att,unefon,twoDegrees)
+                updateAppState(orange, dt)
         );
     }
 
-    private void updateAppState(AppUnderControll...appUnderControll){
+    private void updateAppState(AppUnderControll... appUnderControll) {
         int count = 1;
-        for(AppUnderControll apps : appUnderControll){
+        for (AppUnderControll apps : appUnderControll) {
             Log.d(TAG, "---Now update Group [" + count++ + "] ---");
             apps.setEnableFlag();
             apps.setNewState();
@@ -332,70 +344,89 @@ public class AppStateController {
         }
     }
 
-    private boolean isSubInfoAppropriate(String[] targetSim,String currentSim){
-        if(targetSim == null || targetSim.length == 0 || currentSim == null) return false;
-        for(String sim : targetSim){
-            if(sim.equals(currentSim)) return true;
+    private boolean isSubInfoAppropriate(String[] targetSim, String currentSim) {
+        if (targetSim == null || targetSim.length == 0 || currentSim == null) return false;
+        for (String sim : targetSim) {
+            if (sim.equals(currentSim)) return true;
         }
         return false;
     }
 
-    private boolean isFirstBoot(){
+    private boolean isFirstBoot() {
         try {
             IPackageManager mIPm = IPackageManager.Stub.asInterface(ServiceManager.getService("package"));
             return mIPm.isFirstBoot();
         } catch (RemoteException e) {
-            Log.e(TAG,"FirstBoot? no", e);
+            Log.e(TAG, "FirstBoot? no", e);
             return false;
         }
     }
 
-    private void getSubscriptionInfo(Context context){
-        spn0="";
-        plmn0="";
-        iccid0="";
-        spn1="";
-        plmn1="";
-        iccid1="";
-        if(context == null) return;
+    private void getSubscriptionInfo(Context context) {
+        spn0 = "";
+        plmn0 = "";
+        iccid0 = "";
+        spn1 = "";
+        plmn1 = "";
+        iccid1 = "";
+        gid1_0 = "";
+        gid1_1 = "";
+        if (context == null) return;
         SubscriptionManager subscriptionManager = (SubscriptionManager) context.getSystemService(Context.TELEPHONY_SUBSCRIPTION_SERVICE);
-        if(subscriptionManager == null){
-            Log.i(TAG,"subscriptionManager == null!!!");
+        if (subscriptionManager == null) {
+            Log.i(TAG, "subscriptionManager == null!!!");
             return;
         }
         final List<SubscriptionInfo> subInfoList = subscriptionManager.getActiveSubscriptionInfoList(true);
 
-        TelephonyManager mTelephonyManager = (TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE);
+        TelephonyManager mTelephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
         if (subInfoList != null) {
             final int subInfoLength = subInfoList.size();
-            Log.d(TAG,"subInfoLength = " + subInfoLength);
+            Log.d(TAG, "subInfoLength = " + subInfoLength);
             for (int i = 0; i < subInfoLength; ++i) {
                 final SubscriptionInfo sir = subInfoList.get(i);
-                if(mTelephonyManager != null && sir!=null && isSubIDValid(sir.getSubscriptionId())){
+                if (mTelephonyManager != null && sir != null && isSubIDValid(sir.getSubscriptionId())) {
                     simCount += 1;
                     int subid = sir.getSubscriptionId();
-                    if(i==0){
+                    if (i == 0) {
                         spn0 = mTelephonyManager.getSimOperatorName(subid);
                         plmn0 = mTelephonyManager.getSimOperatorNumeric(subid);
                         iccid0 = mTelephonyManager.getSimSerialNumber(subid);
-                        Log.d(TAG,"spn0= " + spn0 + " plmn0 = " + plmn0 + " iccid0= " + iccid0);
-                    }else if(i==1){
+                        gid1_0 = mTelephonyManager.getGroupIdLevel1(subid);
+                        Log.d(TAG, "spn0= " + spn0 + " plmn0 = " + plmn0 + " iccid0= " + iccid0 + " gid1 = " + gid1_0);
+                    } else if (i == 1) {
                         spn1 = mTelephonyManager.getSimOperatorName(subid);
                         plmn1 = mTelephonyManager.getSimOperatorNumeric(subid);
                         iccid1 = mTelephonyManager.getSimSerialNumber(subid);
-                        Log.d(TAG,"spn1= " + spn1 + " plmn1= " + plmn1 + " iccid1= " + iccid1);
-                    }else{
-                        Log.i(TAG,"so many sim cards!!!");
+                        Log.d(TAG, "spn1= " + spn1 + " plmn1= " + plmn1 + " iccid1= " + iccid1);
+                    } else {
+                        Log.i(TAG, "so many sim cards!!!");
                     }
                 }
             }
-        }else{
-            Log.i(TAG,"subInfoList == null!!!");
+        } else {
+            Log.i(TAG, "subInfoList == null!!!");
+        }
+
+
+        if (getSimLockState() == 1) {
+            if (simCount > 0) {
+                MyCarrierInfo info = new MyCarrierInfo();
+                info.setMccMnc(plmn0);
+                info.setSpn(spn0);
+                info.setGid(gid1_0);
+                saveLockCarrierInfo(info);
+            } else {
+                spn0 = getLockSpn();
+                plmn0 = getLockMccMnc();
+                gid1_0 = getLockGid();
+            }
         }
     }
 
     private boolean hasSimStateChanged = false;
-    private void registerReceiver(){
+
+    private void registerReceiver() {
         IntentFilter mIntentFilter = new IntentFilter();
         mIntentFilter.addAction(TelephonyManager.ACTION_SIM_APPLICATION_STATE_CHANGED);
         //mIntentFilter.addAction(Intent.ACTION_BOOT_COMPLETED);
@@ -403,22 +434,22 @@ public class AppStateController {
         mContext.registerReceiver(new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
-                if(TelephonyManager.ACTION_SIM_APPLICATION_STATE_CHANGED.equals(intent.getAction())){
+                if (TelephonyManager.ACTION_SIM_APPLICATION_STATE_CHANGED.equals(intent.getAction())) {
                     hasSimStateChanged = true;
-                    int simStatus = intent.getIntExtra(TelephonyManager.EXTRA_SIM_STATE,-99);
+                    int simStatus = intent.getIntExtra(TelephonyManager.EXTRA_SIM_STATE, -99);
                     Log.d(TAG, "ACTION_SIM_APPLICATION_STATE_CHANGED" + " simStatus= " + simStatus);
                     updateAppState();
                 }
                 //if(Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction()) && !hasSimStateChanged){
-                if(Intent.ACTION_LOCKED_BOOT_COMPLETED.equals(intent.getAction()) && !hasSimStateChanged){
-                    Log.d(TAG,"LOCKED_BOOT_COMPLETED");
+                if (Intent.ACTION_LOCKED_BOOT_COMPLETED.equals(intent.getAction()) && !hasSimStateChanged) {
+                    Log.d(TAG, "LOCKED_BOOT_COMPLETED");
                     updateAppState();
                 }
             }
         }, mIntentFilter);
     }
 
-    private boolean isAppInstalled(String pkg){
+    private boolean isAppInstalled(String pkg) {
         try {
             ApplicationInfo appInfo = mContext.getPackageManager().getApplicationInfo(pkg, 0);
             if (appInfo != null) {
@@ -434,13 +465,13 @@ public class AppStateController {
         }
     }
 
-    private boolean isSubIDValid(int subid){
+    private boolean isSubIDValid(int subid) {
         int slotID = SubscriptionManager.INVALID_SIM_SLOT_INDEX;
-        if(subid <= SubscriptionManager.INVALID_SUBSCRIPTION_ID){
+        if (subid <= SubscriptionManager.INVALID_SUBSCRIPTION_ID) {
             return false;
         }
         slotID = SubscriptionManager.getSlotIndex(subid);
-        if(slotID == PhoneConstants.SIM_ID_1 || slotID == PhoneConstants.SIM_ID_2){
+        if (slotID == PhoneConstants.SIM_ID_1 || slotID == PhoneConstants.SIM_ID_2) {
             return true;
         }
         return false;
@@ -461,25 +492,25 @@ public class AppStateController {
         private int newState = PackageManager.COMPONENT_ENABLED_STATE_DEFAULT;// 0
         private int enableFlag = PackageManager.DONT_KILL_APP;
 
-        public AppUnderControll(String[] pkgs){
+        public AppUnderControll(String[] pkgs) {
             this.pkgs = pkgs;
         }
 
-        public void setNewState(){
+        public void setNewState() {
             this.newState = isSimAppropriate() ? PackageManager.COMPONENT_ENABLED_STATE_DEFAULT : PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
-            Log.d(TAG,"newState= " + this.newState);
+            Log.d(TAG, "newState= " + this.newState);
         }
 
-        public void setEnableFlag(){
+        public void setEnableFlag() {
 
         }
 
-        public boolean isSimAppropriate(){
-            Log.i(TAG,"override isSimAppropriate()!!!");
+        public boolean isSimAppropriate() {
+            Log.i(TAG, "override isSimAppropriate()!!!");
             return true;
         }
 
-        public void updateAppState(){
+        public void updateAppState() {
             IPackageManager mIPm = IPackageManager.Stub.asInterface(ServiceManager.getService("package"));
             for (String pkg : pkgs) {
                 try {
@@ -487,13 +518,71 @@ public class AppStateController {
                         continue;
                     mIPm.setApplicationEnabledSetting(pkg,
                             newState, enableFlag, USER_SYSTEM, "System");
-                    Log.d(TAG,"set " + pkg + " enable state as " + newState);
-                }catch(Exception e ){
-                    Log.i(TAG,"cannot set " + pkg + " as " + newState);
+                    Log.d(TAG, "set " + pkg + " enable state as " + newState);
+                } catch (Exception e) {
+                    Log.i(TAG, "cannot set " + pkg + " as " + newState);
                 }
             }
         }
 
+    }
+
+
+    private int getSimLockState() {
+        int lockState = SystemProperties.getInt("persist.radio.dsd.locked", 0);
+        Log.d(TAG, " getSimLockState >> " + lockState);
+        return lockState;
+    }
+
+    private String getLockGid() {
+        String lockMccGid = Settings.Global.getString(mContext.getContentResolver(), SETTINGS_CONFIG_SIM_LOCK_GID);
+        return lockMccGid;
+    }
+
+    private String getLockSpn() {
+        String lockMccGid = Settings.Global.getString(mContext.getContentResolver(), SETTINGS_CONFIG_SIM_LOCK_SPN);
+        return lockMccGid;
+    }
+
+    private String getLockMccMnc() {
+        String mccMnc = Settings.Global.getString(mContext.getContentResolver(), SETTINGS_CONFIG_SIM_LOCK_MCCMNC);
+        return mccMnc;
+    }
+
+    private void saveLockCarrierInfo(MyCarrierInfo info) {
+        Settings.Global.putString(mContext.getContentResolver(), SETTINGS_CONFIG_SIM_LOCK_GID, info.getGid());
+        Settings.Global.putString(mContext.getContentResolver(), SETTINGS_CONFIG_SIM_LOCK_MCCMNC, info.getMccMnc());
+        Settings.Global.putString(mContext.getContentResolver(), SETTINGS_CONFIG_SIM_LOCK_SPN, info.getSpn());
+    }
+
+    private class MyCarrierInfo {
+        private String mccMnc;
+        private String gid;
+        private String spn;
+
+        public void setMccMnc(String mccMnc) {
+            this.mccMnc = mccMnc;
+        }
+
+        public void setGid(String gid) {
+            this.gid = gid;
+        }
+
+        public void setSpn(String spn) {
+            this.spn = spn;
+        }
+
+        public String getMccMnc() {
+            return mccMnc;
+        }
+
+        public String getGid() {
+            return gid;
+        }
+
+        public String getSpn() {
+            return spn;
+        }
     }
 
 
