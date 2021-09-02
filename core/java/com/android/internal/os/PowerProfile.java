mer-hybris/android_frameworks_base

Use the battery design capacity exposed by the kernel
Add support for battery packs whose design capacity is exposed by the
kernel. Needed if multiple battery packs with different capacities are
available for the platform as the power profile is statically set with
a fixed value.

FPIIM-2410

Change-Id: Id93d13fb8de63a2375f2948449a380ebb746a166

core/java/com/android/internal/os/PowerProfile.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2009 The Android Open Source Project
+ * Copyright (C) 2018 Fairphone B.V.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,12 +21,16 @@ package com.android.internal.os;
 import android.content.Context;
 import android.content.res.Resources;
 import android.content.res.XmlResourceParser;
+import android.annotation.Nullable;
+import android.util.Log;
 
 import com.android.internal.util.XmlUtils;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -36,6 +41,8 @@ import java.util.HashMap;
  * [hidden]
  */
 public class PowerProfile {
+
+    private static final String TAG = "PowerProfile";
 
     /**
      * No power consumption, or accounted for elsewhere.
@@ -187,6 +194,9 @@ public class PowerProfile {
     private static final String TAG_ARRAYITEM = "value";
     private static final String ATTR_NAME = "name";
 
+    private static final String FILENAME_BATTERY_DESIGN_CAPACITY =
+            "/sys/class/power_supply/bms/charge_full_design";
+
     public PowerProfile(Context context) {
         // Read the XML file for the given profile (normally only one per
         // device)
@@ -279,6 +289,43 @@ public class PowerProfile {
             if (value > 0) {
                 sPowerMap.put(configResIdKeys[i], (double) value);
             }
+        }
+
+        /*
+         * There is no way to set the battery design capacity automatically here from what the
+         * kernel exposes in the sysfs. Several battery packs can be available for the device and
+         * they might have different capacities. So do not rely on the XML power profile and always
+         * use what the kernel exposes instead.
+         */
+        try {
+            // Read the battery design capacity from the sysfs, in microampere-hour
+            int batteryDesignCapacityMicro =
+                    Integer.parseInt(readLine(FILENAME_BATTERY_DESIGN_CAPACITY));
+            // Unconditionally store the sysfs capacity, in milliampere-hour
+            sPowerMap.put(POWER_BATTERY_CAPACITY, ((double) batteryDesignCapacityMicro) / 1000);
+        } catch (IOException ioe) {
+            Log.e(TAG, "Cannot read battery capacity from "
+                    + FILENAME_BATTERY_DESIGN_CAPACITY, ioe);
+        } catch (NumberFormatException nfe) {
+            Log.e(TAG, "Read a badly formatted battery capacity from "
+                    + FILENAME_BATTERY_DESIGN_CAPACITY, nfe);
+        }
+    }
+
+    /**
+    * Reads a line from the specified file.
+    *
+    * @param filename The file to read from.
+    * @return The first line up to 256 characters, or <code>null</code> if file is empty.
+    * @throws IOException If the file couldn't be read.
+    */
+    @Nullable
+    private static String readLine(String filename) throws IOException {
+        final BufferedReader reader = new BufferedReader(new FileReader(filename), 256);
+        try {
+            return reader.readLine();
+        } finally {
+            reader.close();
         }
     }
 
@@ -373,7 +420,7 @@ public class PowerProfile {
     public double getAveragePower(String type) {
         return getAveragePowerOrDefault(type, 0);
     }
-    
+
     /**
      * Returns the average current in mA consumed by the subsystem for the given level.
      * @param type the subsystem type

fairphone-mirror/platform_frameworks_base

Changes in the cherry-pick: Adjust to new data structures in Android 9
(sPowerItemMap instead of sPowerMap).
Issue: FPIIM-2410
Issue: FP2P-109
Issue: FP2P-264
Issue: FP2A10-16
(cherry picked from commit efaa929a0a41c86d866c353042df5d98c73bbbe2)

core/java/com/android/internal/os/PowerProfile.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2009 The Android Open Source Project
+ * Copyright (C) 2018-2021 Fairphone B.V.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,11 +18,13 @@
 package com.android.internal.os;
 
 
+import android.annotation.Nullable;
 import android.annotation.UnsupportedAppUsage;
 import android.content.Context;
 import android.content.res.Resources;
 import android.content.res.XmlResourceParser;
 import android.util.proto.ProtoOutputStream;
+import android.util.Log;
 
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.util.XmlUtils;
@@ -29,6 +32,8 @@ import com.android.internal.util.XmlUtils;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -39,6 +44,8 @@ import java.util.HashMap;
  * [hidden]
  */
 public class PowerProfile {
+
+    private static final String TAG = "PowerProfile";
 
     /*
      * POWER_CPU_SUSPEND: Power consumption when CPU is in power collapse mode.
@@ -236,6 +243,9 @@ public class PowerProfile {
     private static final String TAG_ARRAYITEM = "value";
     private static final String ATTR_NAME = "name";
 
+    private static final String FILENAME_BATTERY_DESIGN_CAPACITY =
+            "/sys/class/power_supply/bms/charge_full_design";
+
     private static final Object sLock = new Object();
 
     @VisibleForTesting
@@ -340,6 +350,43 @@ public class PowerProfile {
             if (value > 0) {
                 sPowerItemMap.put(key, (double) value);
             }
+        }
+
+        /*
+         * There is no way to set the battery design capacity automatically here from what the
+         * kernel exposes in the sysfs. Several battery packs can be available for the device and
+         * they might have different capacities. So do not rely on the XML power profile and always
+         * use what the kernel exposes instead.
+         */
+        try {
+            // Read the battery design capacity from the sysfs, in microampere-hour
+            int batteryDesignCapacityMicro =
+                    Integer.parseInt(readLine(FILENAME_BATTERY_DESIGN_CAPACITY));
+            // Unconditionally store the sysfs capacity, in milliampere-hour
+            sPowerItemMap.put(POWER_BATTERY_CAPACITY, ((double) batteryDesignCapacityMicro) / 1000);
+        } catch (IOException ioe) {
+            Log.e(TAG, "Cannot read battery capacity from "
+                    + FILENAME_BATTERY_DESIGN_CAPACITY, ioe);
+        } catch (NumberFormatException nfe) {
+            Log.e(TAG, "Read a badly formatted battery capacity from "
+                    + FILENAME_BATTERY_DESIGN_CAPACITY, nfe);
+        }
+    }
+
+    /**
+    * Reads a line from the specified file.
+    *
+    * @param filename The file to read from.
+    * @return The first line up to 256 characters, or <code>null</code> if file is empty.
+    * @throws IOException If the file couldn't be read.
+    */
+    @Nullable
+    private static String readLine(String filename) throws IOException {
+        final BufferedReader reader = new BufferedReader(new FileReader(filename), 256);
+        try {
+            return reader.readLine();
+        } finally {
+            reader.close();
         }
     }
 