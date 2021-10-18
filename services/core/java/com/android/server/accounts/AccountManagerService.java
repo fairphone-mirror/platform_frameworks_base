334e6724573950201a70091475b3217a4b8b5576
Maarten Derks <maarten@fairphone.com>
2021-10-18 09:59:31 +0200

    Revert "Change ownership of the account request notification."
    This reverts commit 38a939d11f5d92a373795e16df7c37cf34544f55.
    
    Patch has been removed from the 2021-10-01 SPL to address an
    incomplete fix.
    
    Issue: SEC-3090
    Change-Id: I94e29f3df87fd84f56deac33626fac40673dd79d

--- services/core/java/com/android/server/accounts/AccountManagerService.java
+++ services/core/java/com/android/server/accounts/AccountManagerService.java
@@ -452,7 +452,7 @@ public class AccountManagerService
         if (!checkAccess || hasAccountAccess(account, packageName,
                 UserHandle.getUserHandleForUid(uid))) {
             cancelNotification(getCredentialPermissionNotificationId(account,
-                    AccountManager.ACCOUNT_ACCESS_TOKEN_TYPE, uid),
+                    AccountManager.ACCOUNT_ACCESS_TOKEN_TYPE, uid), packageName,
                     UserHandle.getUserHandleForUid(uid));
         }
     }
@@ -3035,8 +3035,8 @@ public class AccountManagerService
         String authTokenType = intent.getStringExtra(
                 GrantCredentialsPermissionActivity.EXTRAS_AUTH_TOKEN_TYPE);
         final String titleAndSubtitle =
-                mContext.getString(R.string.permission_request_notification_for_app_with_subtitle,
-                getApplicationLabel(packageName), account.name);
+                mContext.getString(R.string.permission_request_notification_with_subtitle,
+                account.name);
         final int index = titleAndSubtitle.indexOf('\n');
         String title = titleAndSubtitle;
         String subtitle = "";
@@ -3058,16 +3058,7 @@ public class AccountManagerService
                             PendingIntent.FLAG_CANCEL_CURRENT, null, user))
                     .build();
         installNotification(getCredentialPermissionNotificationId(
-                account, authTokenType, uid), n, "android", user.getIdentifier());
-    }
-
-    private String getApplicationLabel(String packageName) {
-        try {
-            return mPackageManager.getApplicationLabel(
-                    mPackageManager.getApplicationInfo(packageName, 0)).toString();
-        } catch (PackageManager.NameNotFoundException e) {
-            return packageName;
-        }
+                account, authTokenType, uid), n, packageName, user.getIdentifier());
     }
 
     private Intent newGrantCredentialsPermissionIntent(Account account, String packageName,
@@ -3103,7 +3094,7 @@ public class AccountManagerService
             nId = accounts.credentialsPermissionNotificationIds.get(key);
             if (nId == null) {
                 String tag = TAG + ":" + SystemMessage.NOTE_ACCOUNT_CREDENTIAL_PERMISSION
-                        + ":" + account.hashCode() + ":" + authTokenType.hashCode() + ":" + uid;
+                        + ":" + account.hashCode() + ":" + authTokenType.hashCode();
                 int id = SystemMessage.NOTE_ACCOUNT_CREDENTIAL_PERMISSION;
                 nId = new NotificationId(tag, id);
                 accounts.credentialsPermissionNotificationIds.put(key, nId);
@@ -4056,7 +4047,7 @@ public class AccountManagerService
 
             private void handleAuthenticatorResponse(boolean accessGranted) throws RemoteException {
                 cancelNotification(getCredentialPermissionNotificationId(account,
-                        AccountManager.ACCOUNT_ACCESS_TOKEN_TYPE, uid),
+                        AccountManager.ACCOUNT_ACCESS_TOKEN_TYPE, uid), packageName,
                         UserHandle.getUserHandleForUid(uid));
                 if (callback != null) {
                     Bundle result = new Bundle();
