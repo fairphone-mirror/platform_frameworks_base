d47d4fe39e02690ff63d0cb5bee09587eedec4e5
xiaoyang.wang <xiaoyang.wang@t2mobile.com>
2022-01-04 19:34:03 +0800

    FP4-2764 [Monitor][Sound][Custom] After selecting a song from DUT, it does not highlight on Phone ringtone screen
    Change-Id: I6453e6db100b9094a6a2f7e684e79d59289f0804

--- packages/SoundPicker/src/com/android/soundpicker/RingtonePickerActivity.java
+++ packages/SoundPicker/src/com/android/soundpicker/RingtonePickerActivity.java
@@ -485,15 +485,20 @@ public final class RingtonePickerActivity extends AlertActivity implements
             final long ringtoneId = ContentUris.parseId(ringtoneUri);
 
             if (cursor != null) {
+                Log.i(TAG,"cursor.moveToPosition(-1)");
                 cursor.moveToPosition(-1);
+            }else {
+                Log.i(TAG,"cursor != null return -1");
+                return -1;
             }
 
             while (cursor.moveToNext()) {
-//            Log.d(TAG, "title: " + cursor.getString(RingtoneManager.TITLE_COLUMN_INDEX));
+            Log.d(TAG, "title: " + cursor.getString(RingtoneManager.TITLE_COLUMN_INDEX));
                 if (ringtoneId == cursor.getLong(RingtoneManager.ID_COLUMN_INDEX)) {
-                    String uriString = cursor.getString(RingtoneManager.URI_COLUMN_INDEX);
-                    final Uri uri = Uri.parse(uriString);
+                    // String uriString = cursor.getString(RingtoneManager.URI_COLUMN_INDEX);
+                    // final Uri uri = Uri.parse(uriString);
 //                if (isExternalRingtoneUri(uri)) {
+                    Log.d(TAG, "title: " + cursor.getString(RingtoneManager.TITLE_COLUMN_INDEX)+"   cursor.getPosition():"+cursor.getPosition());
                     return cursor.getPosition();
 //                }
                 }
