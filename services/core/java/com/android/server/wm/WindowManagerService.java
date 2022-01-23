272998a047b948615aceaf26d949627931f27c1c
Robert Carr <racarr@google.com>
2022-01-22 16:49:31 -0800

    DO NOT MERGE: WM: Call Transaction#sanitize
    Various elements of the Transaction interface require
    a permission in order to apply. In particular the setTrustedOverlay
    and setInputWindowInfo fields. These permission checks are
    implemented by checking the PID and the UID of the process which
    sent the transaction. Unfortunately widespread use of transaction
    merging makes this inadequate. At the moment
    IWindowSession#finishDrawing seems to be the only boundary on which
    transactions move from client to system processes, and so we expose
    a sanitize method and use it from there to resolve the situation
    in an easily backportable way.
    
    Moving forward it likely make sense to move security sensitive
    interfaces off of Transaction. Most of the things behind permissions
    currently are not truly security sensitive, more of just a request
    not to use them.
    
    It was also considered to sanitize transactions at all process
    boundaries through writeToParcel, however this could be disruptive
    as previously permissioned processes (WM and SysUI) could freely
    exchange transactions. As the change needs to be backportable the
    lowest risk option was chosen.
    
    Bug: 213644870
    Test: Existing tests pass
    Change-Id: I8d4a0ebe0cdfaed7ff1ad862724d49a14ed04478
    (cherry picked from commit cdf139841567a625608b94339fa49cb895f6a69f)
    (cherry picked from commit c54faf3e691b3d952f649756578eab6f8a5d3208)
    (cherry picked from commit bc798bb43287674668d15656325018a0f4dd45de)
    Merged-In: I8d4a0ebe0cdfaed7ff1ad862724d49a14ed04478

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -2646,6 +2646,10 @@ public class WindowManagerService extends IWindowManager.Stub
 
     void finishDrawingWindow(Session session, IWindow client,
             @Nullable SurfaceControl.Transaction postDrawTransaction) {
+        if (postDrawTransaction != null) {
+            postDrawTransaction.sanitize();
+        }
+
         final long origId = Binder.clearCallingIdentity();
         try {
             synchronized (mGlobalLock) {
