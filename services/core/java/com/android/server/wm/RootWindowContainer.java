3b4921431724437b1bf900f976648be1a34dc2ba
Jeff Chang <chengjeff@google.com>
2022-01-25 11:57:46 +0800

    Do not resume activity if behind a translucent task
    The top-focusable activity resides in the RESUMED state while the app
    process is newly created and attached. The behavior may enable UI
    hijacking attacks against apps implementing authentication.
    
    This CL disallows the system to resume the activity for the case if it
    is not visible or is occluded by other translucent tasks.
    
    Bug: 211481342
    Test: atest CtsWindowManagerDeviceTestCases:ActivityLifecycleTests
    Change-Id: I7903494cf928b5b5613700262b7c5fff10f3c5a0
    (cherry picked from commit 51d287d831364cb158330e132cd11b4c596c04c2)
    Merged-In: I7903494cf928b5b5613700262b7c5fff10f3c5a0

--- services/core/java/com/android/server/wm/RootWindowContainer.java
+++ services/core/java/com/android/server/wm/RootWindowContainer.java
@@ -1956,7 +1956,8 @@ public class RootWindowContainer extends WindowContainer<DisplayContent>
 
         try {
             if (mTaskSupervisor.realStartActivityLocked(r, app,
-                    top == r && r.isFocusable() /*andResume*/, true /*checkConfig*/)) {
+                    top == r && r.getTask().canBeResumed(r) /*andResume*/,
+                    true /*checkConfig*/)) {
                 mTmpBoolean = true;
             }
         } catch (RemoteException e) {
