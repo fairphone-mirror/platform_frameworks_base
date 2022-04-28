de838958591b37a1a5c47b40761dc10484f8314c
Linus Tufvesson <lus@google.com>
2022-04-28 16:35:56 +0200

    Make Activites touch opaque - DO NOT MERGE
    Block touches from passing through activities by adding a dedicated
    surface that consumes all touches that would otherwise pass through the
    bounds availble to the Activity.
    
    Bug: 194480991
    Test: atest CtsWindowManagerDeviceTestCases:ActivityRecordInputSinkTests
    Test: atest CtsWindowManagerDeviceTestCases:CrossAppDragAndDropTests
    Test: atest CtsWindowManagerDeviceTestCases:PinnedStackTests
    Test: Manually ran through go/wm-smoke (verified pip and splitscreen
    still work)
    Test: Manually verfied that freeform windows work and don't consume
    touches outside their bounds
    
    Change-Id: I01b35e34a63868dead4e728a3d359ae0942302f9
    (cherry picked from commit 22261fa6649f6ec6441646743ad98132fcf47fe0)
    Merged-In: I01b35e34a63868dead4e728a3d359ae0942302f9

--- services/core/java/com/android/server/wm/ActivityRecord.java
+++ services/core/java/com/android/server/wm/ActivityRecord.java
@@ -787,6 +787,13 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
 
     private AppSaturationInfo mLastAppSaturationInfo;
 
+    private final ActivityRecordInputSink mActivityRecordInputSink;
+
+    // Activities with this uid are allowed to not create an input sink while being in the same
+    // task and directly above this ActivityRecord. This field is updated whenever a new activity
+    // is launched from this ActivityRecord. Touches are always allowed within the same uid.
+    int mAllowedTouchUid;
+
     private final ColorDisplayService.ColorTransformController mColorTransformController =
             (matrix, translation) -> mWmService.mH.post(() -> {
                 synchronized (mWmService.mGlobalLock) {
@@ -1738,6 +1745,8 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
 
         if (mPerf == null)
             mPerf = new BoostFramework();
+
+        mActivityRecordInputSink = new ActivityRecordInputSink(this, sourceRecord);
     }
 
     /**
@@ -3584,6 +3593,7 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
             destroyImmediately("removeImmediately");
         }
         onRemovedFromDisplay();
+        mActivityRecordInputSink.releaseSurfaceControl();
         super.removeImmediately();
     }
 
@@ -6813,6 +6823,9 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
                 getSyncTransaction().show(mSurfaceControl);
             } else if (!show && mLastSurfaceShowing) {
                 getSyncTransaction().hide(mSurfaceControl);
+            }
+            if (show) {
+                mActivityRecordInputSink.applyChangesToSurfaceIfChanged(getSyncTransaction());
             }
         }
         if (mThumbnail != null) {
