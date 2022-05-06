783fa9bbd155ab945979564c3030e87c172be729
Noorul Irfan <noorul.irfan@sony.com>
2022-05-06 11:46:12 +0000

    Crash when setting config_showOperatorNameInStatusBar as true
    When typecasting customized textview into OperatorNameView
    ClassCastException occurs. This is due to both viewStub
    and Textview present inside viewStub having same id.
    While typecasting, instead of child OperatorNameView,
    parent viewStub is been used.
    
    Hence fixing this issue by fetching the OperatorNameView
    from viewstub.
    
    In addition, changing the ViewStub name into "operator_name_stub"
    in both xml and java files.
    
    Issue: FP3-A13#48
    Test: Manual
    Bug: TBD
    Change-Id: Idf7c94a5a3e9e5ad637bef3d3031fa4ae65af710
    (cherry picked from commit b4ac9dd5d7139a2dac8a923bff68ee8c334c9a07)

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
@@ -535,9 +535,10 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
 
     private void initOperatorName() {
         if (getResources().getBoolean(R.bool.config_showOperatorNameInStatusBar)) {
-            ViewStub stub = mStatusBar.findViewById(R.id.operator_name);
+            ViewStub stub = mStatusBar.findViewById(R.id.operator_name_stub);
             mOperatorNameViewController =
-                    mOperatorNameViewControllerFactory.create((OperatorNameView) stub.inflate());
+                    mOperatorNameViewControllerFactory.create((OperatorNameView) stub.inflate()
+                    .findViewById(R.id.operator_name));
             mOperatorNameViewController.init();
         }
     }
