de446297e9a8fc508c2f3de260060621a82763fd
yubin.ying <yubin.ying@t2mobile.com>
2022-05-24 17:51:38 +0800

    [FP4S-81] [Security]auto wipe after unlock failed

--- packages/SystemUI/src/com/android/keyguard/KeyguardSecurityContainerController.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardSecurityContainerController.java
@@ -53,6 +53,7 @@ import com.android.systemui.shared.system.SysUiStatsLog;
 import com.android.systemui.statusbar.policy.ConfigurationController;
 import com.android.systemui.statusbar.policy.KeyguardStateController;
 import com.android.systemui.util.ViewController;
+import android.provider.Settings;
 
 import javax.inject.Inject;
 
@@ -78,6 +79,8 @@ public class KeyguardSecurityContainerController extends ViewController<Keyguard
     private int mLastOrientation = Configuration.ORIENTATION_UNDEFINED;
 
     private SecurityMode mCurrentSecurityMode = SecurityMode.Invalid;
+
+    public static final String KEY_UNLOCK_FAILED_RESET = "unlock_failed_reset";
 
     private final Gefingerpoken mGlobalTouchListener = new Gefingerpoken() {
         private MotionEvent mTouchDown;
@@ -153,6 +156,9 @@ public class KeyguardSecurityContainerController extends ViewController<Keyguard
             } else {
                 SysUiStatsLog.write(SysUiStatsLog.KEYGUARD_BOUNCER_PASSWORD_ENTERED,
                         SysUiStatsLog.KEYGUARD_BOUNCER_PASSWORD_ENTERED__RESULT__FAILURE);
+                if (resetForFailedUnlock(userId)) {
+                    return;
+                }
                 reportFailedUnlockAttempt(userId, timeoutMs);
             }
             mMetricsLogger.write(new LogMaker(MetricsEvent.BOUNCER)
@@ -491,6 +497,31 @@ public class KeyguardSecurityContainerController extends ViewController<Keyguard
             mView.showTimeoutDialog(userId, timeoutMs, mLockPatternUtils,
                     mSecurityModel.getSecurityMode(userId));
         }
+    }
+
+    private boolean resetForFailedUnlock(int userId) {
+        final int failedAttempts = mLockPatternUtils.getCurrentFailedPasswordAttempts(userId);
+        if (failedAttempts >= 14) {
+            boolean unlockFaileReset = Settings.Global.getInt(
+                    getContext().getContentResolver(),
+                    KEY_UNLOCK_FAILED_RESET, 0) == 1;
+            if (DEBUG) Log.d(TAG, "resetForFailedUnlock: # " + unlockFaileReset);
+            if (unlockFaileReset) {
+                doMasterClear();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void doMasterClear() {
+        Intent intent = new Intent(Intent.ACTION_FACTORY_RESET);
+        intent.setPackage("android");
+        intent.addFlags(Intent.FLAG_RECEIVER_FOREGROUND);
+        intent.putExtra(Intent.EXTRA_REASON, "MasterClearConfirm");
+        //        intent.putExtra(Intent.EXTRA_WIPE_EXTERNAL_STORAGE, mEraseSdCard);
+        getContext().sendBroadcast(intent);
+        //  Intent handling is asynchronous -- assume it will happen soon.
     }
 
     private KeyguardInputViewController<KeyguardInputView> getCurrentSecurityController() {
