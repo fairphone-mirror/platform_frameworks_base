3330e67d1002cdb8acda7f41bc1a44c3ba6e0ab6
yubin.ying <yubin.ying@t2mobile.com>
2022-05-25 14:09:07 +0800

    [FP4S-71] There is no unlock prompt message and emergency button on the lock screen

--- packages/SystemUI/src/com/android/keyguard/KeyguardPinViewController.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardPinViewController.java
@@ -78,7 +78,7 @@ public class KeyguardPinViewController
     @Override
     void resetState() {
         super.resetState();
-        mMessageAreaController.setMessage("");
+        mMessageAreaController.setMessage(R.string.keyguard_enter_your_pin);
     }
 
     @Override
