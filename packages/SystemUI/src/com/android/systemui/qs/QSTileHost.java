5753a04791d7cc161a3b5e7949825cc09e1d1102
xiujuan.yuan <xiujuan.yuan@t2mobile.com>
2022-06-27 17:51:51 +0800

    [FP4S-237] [Wifi calling]Long press cannot enter the wifi-calling setting
    Change-Id: I710f857c3f7e06ce52dadcce9075a5e88cbb29b6

--- packages/SystemUI/src/com/android/systemui/qs/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSTileHost.java
@@ -82,7 +82,7 @@ import javax.inject.Provider;
 @SysUISingleton
 public class QSTileHost implements QSHost, Tunable, PluginListener<QSFactory>, Dumpable {
     private static final String TAG = "QSTileHost";
-    private static final boolean DEBUG = Log.isLoggable(TAG, Log.DEBUG);
+    private static final boolean DEBUG = true;
     private static final int MAX_QS_INSTANCE_ID = 1 << 20;
 
     public static final int POSITION_AT_END = -1;
@@ -566,9 +566,12 @@ public class QSTileHost implements QSHost, Tunable, PluginListener<QSFactory>, D
                     addedDefault = true;
                 }
             } else {
-                if (!addedSpecs.contains(tile)) {
-                    tiles.add(tile);
-                    addedSpecs.add(tile);
+                if(!tile.equals("custom(com.android.settings/.qs.VoWifiTile)")){
+                    if (!addedSpecs.contains(tile)) {
+                        Log.e(TAG,"SELF  " + tile);
+                        tiles.add(tile);
+                        addedSpecs.add(tile);
+                    }
                 }
             }
         }
@@ -586,6 +589,10 @@ public class QSTileHost implements QSHost, Tunable, PluginListener<QSFactory>, D
             tiles.remove("wifi");
             tiles.remove("cell");
         }
+
+        //if(tiles.contains("VoWifiTile")){
+            tiles.remove("custom(com.android.settings/.qs.VoWifiTile)");
+        //}
         return tiles;
     }
 
