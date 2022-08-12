af8c5b3a110bc3a33a17c998d38b5cd4a3b9ec81
xiaoyang.wang <xiaoyang.wang@t2mobile.com>
2022-08-12 14:13:37 +0800

    FP4S-480 Add code protection
    Change-Id: I2f4aaef5e74120a89b7c9a6b5217020ae09279ee

--- services/core/java/com/android/server/PinnerService.java
+++ services/core/java/com/android/server/PinnerService.java
@@ -357,7 +357,11 @@ public final class PinnerService extends SystemService {
                         if (userSetupCompleteUri.equals(uri)) {
                             sendPinAppMessage(KEY_HOME, ActivityManager.getCurrentUser(),
                                     true /* force */);
-                            startMyFairphone();
+                            try{
+                                startMyFairphone();
+                            } catch (Exception e) {
+                                Slog.e(TAG, "Failed find MyFirePhone ", e);
+                            }
                         }
                     }
                 }, UserHandle.USER_ALL);
