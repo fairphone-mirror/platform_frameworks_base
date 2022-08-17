c0006f07f2111eee75ba69ebc0a7e7889bdaf6d4
xiaoyang.wang <xiaoyang.wang@t2mobile.com>
2022-08-17 17:17:59 +0800

    FP4S-375 [Lock Screen][Camera]Can not launch camera from lock screen by slide camera icon(修复锁屏双击power无法跳转camera)
    Change-Id: I26ba6fc1da23ffad942abb287bbc4cbc19e0ea41

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarKeyguardViewManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarKeyguardViewManager.java
@@ -484,6 +484,7 @@ public class StatusBarKeyguardViewManager implements RemoteInputController.Callb
         if (mShowing) {
             // If we were showing the bouncer and then aborting, we need to also clear out any
             // potential actions unless we actually unlocked.
+            mAfterKeyguardGoneAction = null;
             cancelPostAuthActions();
         }
         cancelPendingWakeupAction();
@@ -1013,7 +1014,6 @@ public class StatusBarKeyguardViewManager implements RemoteInputController.Callb
         if (bouncerIsOrWillBeShowing()) {
             return; // allow bouncer to trigger saved actions
         }
-        mAfterKeyguardGoneAction = null;
         mDismissActionWillAnimateOnKeyguard = false;
         if (mKeyguardGoneCancelAction != null) {
             mKeyguardGoneCancelAction.run();
