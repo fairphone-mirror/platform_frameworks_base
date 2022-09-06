a3ab7fa45124accb6b81604daced2bf74f17b6a6
xiaoyang.wang <xiaoyang.wang@t2mobile.com>
2022-09-06 09:21:31 +0800

    FP4S-544 [Quick setting]There is no nfc menu in quick setting(添加NFC图标)
    Change-Id: I80e6aa5427dd36ed33898126ebfc3a5cb79e5213

--- packages/SystemUI/src/com/android/systemui/qs/tiles/NfcTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/NfcTile.java
@@ -159,7 +159,7 @@ public class NfcTile extends QSTileImpl<BooleanState> {
     private NfcAdapter getAdapter() {
         if (mAdapter == null) {
             try {
-                mAdapter = NfcAdapter.getDefaultAdapter();
+                mAdapter = NfcAdapter.getDefaultAdapter(mContext);
             } catch (UnsupportedOperationException e) {
                 mAdapter = null;
             }
