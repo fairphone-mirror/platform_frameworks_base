07bf37f87249604afe6100f54f3411f3b374ce5a
xiujuan.yuan <xiujuan.yuan@t2mobile.com>
2022-11-16 18:12:27 +0800

    [FP4S-708] [Work Device] Carrier dependent applications are incorrect installed for Work only device.
    Change-Id: I4040f05a4a647082bb4e756f44551925cb9a29d8

--- services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
+++ services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
@@ -11623,8 +11623,10 @@ public class DevicePolicyManagerService extends BaseIDevicePolicyManager {
     @Override
     public void enableSystemApp(ComponentName who, String callerPackage, String packageName) {
 
+        android.util.Log.e("xiujuan1"," enableSystemApp: callerPackage:" + callerPackage + " packageName" + packageName);
         if(WORK_PACKAGE_NAME.equals(callerPackage)){
             if (CARRIER_PREINSTALL_LIST.contains(packageName)) {
+                android.util.Log.e("xiujuan2"," enableSystemApp: callerPackage:" + callerPackage + " packageName" + packageName);
                 return;
             }
         }
