1ac1fd1f809d1f6863f78065caf07be41b3243c6
Karsten Tausche <karsten@fairphone.com>
2022-12-16 17:47:11 +0100

    Revert "Set proper default state of Mobile Data connection [1/2]"
    This reverts commit b12c7be4fef4fe3e68237349fc3ad338808e8d40.
    
    This was only required for legacy mobile data setup wizard page; it was
    dropped during Android 11 development. Drop this leftover commit here as
    well.
    
    Issue: FP3-A13#26
    Change-Id: Ia7cf614b804717d0ad23e7540ef9acf4f17e0594

--- telephony/java/android/telephony/CarrierConfigManager.java
+++ telephony/java/android/telephony/CarrierConfigManager.java
@@ -1967,13 +1967,6 @@ public class CarrierConfigManager {
             "always_show_data_rat_icon_bool";
 
     /**
-     * Boolean indicating if show mobile data need to be enabled
-     * @hide
-     */
-    public static final String KEY_CARRIER_DEFAULT_DATA_ENABLED_BOOL =
-            "carrier_default_mobile_data_enabled_bool";
-
-    /**
      * Boolean indicating if default data account should show LTE or 4G icon.
      */
     public static final String KEY_SHOW_4G_FOR_LTE_DATA_ICON_BOOL =
@@ -9252,7 +9245,6 @@ public class CarrierConfigManager {
         sDefaults.putBoolean(KEY_USE_ACS_FOR_RCS_BOOL, false);
         sDefaults.putBoolean(KEY_NETWORK_TEMP_NOT_METERED_SUPPORTED_BOOL, true);
         sDefaults.putInt(KEY_DEFAULT_RTT_MODE_INT, 0);
-        sDefaults.putBoolean(KEY_CARRIER_DEFAULT_DATA_ENABLED_BOOL, false);
         sDefaults.putBoolean(KEY_STORE_SIM_PIN_FOR_UNATTENDED_REBOOT_BOOL, true);
         sDefaults.putBoolean(KEY_HIDE_ENABLE_2G, false);
         sDefaults.putStringArray(KEY_ALLOWED_INITIAL_ATTACH_APN_TYPES_STRING_ARRAY,
