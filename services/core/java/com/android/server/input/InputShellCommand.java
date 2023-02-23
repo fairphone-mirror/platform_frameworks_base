65580ed51ac18bcf6437a08602b41e4c3afa5a79
chuanzhi.shao <chuanzhi.shao@t2mobile.com>
2023-02-23 17:42:50 +0800

    FP5-163 Fix adb shell input keycode = 0
    Change-Id: Ifa363f571694944f6588314e741212ae85570a2d

--- services/core/java/com/android/server/input/InputShellCommand.java
+++ services/core/java/com/android/server/input/InputShellCommand.java
@@ -341,7 +341,8 @@ public class InputShellCommand extends ShellCommand {
         }
 
         do {
-            final int keycode = KeyEvent.keyCodeFromString(arg);
+            // final int keycode = KeyEvent.keyCodeFromString(arg);
+            final int keycode = KeyEvent.fpKeyCodeFromString(arg);
             sendKeyEvent(inputSource, keycode, longpress, displayId);
         } while ((arg = getNextArg()) != null);
     }
