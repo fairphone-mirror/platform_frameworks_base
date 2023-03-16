fairphone-mirror/platform_frameworks_base

commit 53e9f4df475470c82edf2e52e43a73ae09be8dd0

Operator: Implement various UI requirements [2/2]
* Vodafone and Bouygues Telecom.

Issue: FP3-A11#231
Change-Id: I889d075fa08d9909f9eb966222f0775c8b05f54a
Depends-On: I005f1af9ec8dfdc56775412bf71c4445d4ce7602
(cherry picked from commit 07eb8668cd9816d3b9c38a0e00a8cde916e68c14)

packages/SettingsLib/src/com/android/settingslib/mobile/TelephonyIcons.java
@@ -37,6 +37,7 @@ public class TelephonyIcons {
     public static final int ICON_H = R.drawable.ic_h_mobiledata;
     public static final int ICON_H_PLUS = R.drawable.ic_h_plus_mobiledata;
     public static final int ICON_3G = R.drawable.ic_3g_mobiledata;
+    public static final int ICON_3G_PLUS = R.drawable.ic_3g_plus_mobiledata;
     public static final int ICON_4G = R.drawable.ic_4g_mobiledata;
     public static final int ICON_4G_PLUS = R.drawable.ic_4g_plus_mobiledata;
     public static final int ICON_4G_LTE = R.drawable.ic_4g_lte_mobiledata;
@@ -73,6 +74,20 @@ public class TelephonyIcons {
             AccessibilityContentDescriptions.PHONE_SIGNAL_STRENGTH[0],
             R.string.data_connection_3g,
             TelephonyIcons.ICON_3G
+    );
+
+    public static final MobileIconGroup THREE_G_PLUS = new MobileIconGroup(
+            "3G+",
+            null,
+            null,
+            AccessibilityContentDescriptions.PHONE_SIGNAL_STRENGTH,
+            0,
+            0,
+            0,
+            0,
+            AccessibilityContentDescriptions.PHONE_SIGNAL_STRENGTH[0],
+            R.string.data_connection_3g,
+            TelephonyIcons.ICON_3G_PLUS
     );
 
     public static final MobileIconGroup WFC = new MobileIconGroup(

core/java/android/app/ApplicationPackageManager.java
@@ -172,6 +172,26 @@ public class ApplicationPackageManager extends PackageManager {
     private volatile DevicePolicyManager mDevicePolicyManager;
     private volatile String mPermissionsControllerPackageName;
 
+    private static final String PROP_GMS_SIM_OPERATOR_NUMERIC = "gsm.sim.operator.numeric";
+
+    private static final ArrayList<String> VODAFONE_NUMERICS =
+            new ArrayList<>(
+                    List.of(
+                            "26202", // Germany
+                            "26209", // Germany
+                            "23415", // UK
+                            "22210", // Italy
+                            "21401", // Spain
+                            "27201", // Ireland
+                            "20404", // Netherlands
+                            "26801", // Portugal
+                            "20205", // Greece
+                            "22801" // Switzerland
+                            ));
+    private static final String STK_VODAFONE_TEXT = "Vodafone";
+
+    private static final String STK_PACKAGE_NAME = "com.android.stk";
+
     @GuardedBy("mDelegates")
     private final ArrayList<MoveCallbackDelegate> mDelegates = new ArrayList<>();
 
@@ -2322,6 +2342,11 @@ public class ApplicationPackageManager extends PackageManager {
         ResourceName name = new ResourceName(packageName, resid);
         CharSequence text = getCachedString(name);
         if (text != null) {
+            if (STK_PACKAGE_NAME.equalsIgnoreCase(packageName)) {
+                if (isVodafoneSIM()) {
+                    text = STK_VODAFONE_TEXT;
+                }
+            }
             return text;
         }
         if (appInfo == null) {
@@ -2335,6 +2360,11 @@ public class ApplicationPackageManager extends PackageManager {
             Resources r = getResourcesForApplication(appInfo);
             text = r.getText(resid);
             putCachedString(name, text);
+            if (STK_PACKAGE_NAME.equalsIgnoreCase(packageName)) {
+                if (isVodafoneSIM()) {
+                    text = STK_VODAFONE_TEXT;
+                }
+            }
             return text;
         } catch (NameNotFoundException e) {
             Log.w("PackageManager", "Failure retrieving resources for "
@@ -2347,6 +2377,18 @@ public class ApplicationPackageManager extends PackageManager {
                   + packageName, e);
         }
         return null;
+    }
+
+    private static boolean isVodafoneSIM() {
+        String numeric = SystemProperties.get(PROP_GMS_SIM_OPERATOR_NUMERIC);
+        if (numeric == null) {
+            return false;
+        }
+        numeric = numeric.replaceAll(",", "");
+        if (numeric.length() < 5) {
+            return false;
+        }
+        return VODAFONE_NUMERICS.contains(numeric.substring(0, 5));
     }
 
     @Override

core/res/res/values/strings.xml
@@ -5906,6 +5906,8 @@
     <!-- ChooserActivity - Alphabetically sorted apps list label. [CHAR LIMIT=NONE] -->
     <string name="chooser_all_apps_button_label">Apps list</string>
 
+    <string name="sim_default_name">CARD "<xliff:g id="slot_id">%1$s</xliff:g>"</string>
+
     <!-- Prompt for the USB device resolver dialog with warning text for USB device dialogs.  [CHAR LIMIT=200] -->
     <string name="usb_device_resolve_prompt_warn">This app has not been granted record permission but could capture audio through this USB device.</string>
 

core/res/res/values/symbols.xml
@@ -4801,4 +4801,7 @@
   <java-symbol type="id" name="language_picker_header" />
 
   <java-symbol type="dimen" name="status_bar_height_default" />
+
+  <!-- Custom -->
+  <java-symbol type="string" name="sim_default_name" />
 </resources>

packages/SettingsLib/res/values/config.xml
@@ -28,4 +28,6 @@
     <!-- Control whether status bar should distinguish HSPA data icon form UMTS
     data icon on devices -->
     <bool name="config_hspa_data_distinguishable">false</bool>
+
+    <bool name="config_hspa_data_btb_customized">false</bool>
 </resources>

packages/SettingsLib/src/com/android/settingslib/mobile/MobileMappings.java
@@ -90,6 +90,16 @@ public class MobileMappings {
     public static Map<String, MobileIconGroup> mapIconSets(Config config) {
         final Map<String, MobileIconGroup> networkToIconLookup = new HashMap<>();
 
+        networkToIconLookup.put(toIconKey(TelephonyManager.NETWORK_TYPE_GPRS),
+                TelephonyIcons.G);
+
+        if (config.hspaBTBCustomized) {
+            networkToIconLookup.put(toIconKey(TelephonyManager.NETWORK_TYPE_HSPA),
+                    TelephonyIcons.THREE_G_PLUS);
+            networkToIconLookup.put(toIconKey(TelephonyManager.NETWORK_TYPE_HSPAP),
+                    TelephonyIcons.H_PLUS);
+        }
+
         networkToIconLookup.put(toIconKey(TelephonyManager.NETWORK_TYPE_EVDO_0),
                 TelephonyIcons.THREE_G);
         networkToIconLookup.put(toIconKey(TelephonyManager.NETWORK_TYPE_EVDO_A),
@@ -217,6 +227,7 @@ public class MobileMappings {
         public boolean hideLtePlus = false;
         public boolean hspaDataDistinguishable;
         public boolean alwaysShowDataRatIcon = false;
+        public boolean hspaBTBCustomized = false;
 
         /**
          * Reads the latest configs.
@@ -230,6 +241,8 @@ public class MobileMappings {
                     res.getBoolean(com.android.internal.R.bool.config_alwaysUseCdmaRssi);
             config.hspaDataDistinguishable =
                     res.getBoolean(R.bool.config_hspa_data_distinguishable);
+            config.hspaBTBCustomized =
+                    res.getBoolean(R.bool.config_hspa_data_btb_customized);
 
             CarrierConfigManager configMgr = (CarrierConfigManager)
                     context.getSystemService(Context.CARRIER_CONFIG_SERVICE);

packages/SystemUI/res/drawable/ic_vowifi_v2_white.xml
@@ -1,0 +1,15 @@
+<vector android:height="17dp" android:viewportHeight="24"
+    android:viewportWidth="24" android:width="17dp" xmlns:android="http://schemas.android.com/apk/res/android">
+    <path android:fillAlpha="0" android:fillColor="#000000" android:pathData="M0,0L24,0L24,24L0,24L0,0Z"/>
+    <path android:fillAlpha="0" android:fillColor="#FF000000"
+        android:pathData="M0,0L24,0L24,24L0,24L0,0Z"
+        android:strokeAlpha="0" android:strokeColor="#000000" android:strokeWidth="1"/>
+    <path android:fillAlpha="1" android:fillColor="#fffcfc" android:pathData="M4.64,7.82C8.7,3.85 15.3,3.85 19.36,7.82C19.53,7.66 20.84,6.38 21,6.22C16.03,1.37 7.97,1.37 3,6.22C3.33,6.54 4.47,7.66 4.64,7.82ZM12,15C13.47,13.56 14.29,12.76 14.45,12.61C13.1,11.28 10.9,11.28 9.55,12.61C9.87,12.92 10.69,13.72 12,15ZM7.91,11.01C10.17,8.81 13.83,8.81 16.09,11.01C16.25,10.85 17.56,9.57 17.73,9.41C14.57,6.33 9.44,6.33 6.27,9.41C6.6,9.73 7.75,10.85 7.91,11.01Z"/>
+    <path android:fillAlpha="0" android:fillColor="#FF000000"
+        android:pathData="M4.64,7.82C8.7,3.85 15.3,3.85 19.36,7.82C19.53,7.66 20.84,6.38 21,6.22C16.03,1.37 7.97,1.37 3,6.22C3.33,6.54 4.47,7.66 4.64,7.82ZM12,15C13.47,13.56 14.29,12.76 14.45,12.61C13.1,11.28 10.9,11.28 9.55,12.61C9.87,12.92 10.69,13.72 12,15ZM7.91,11.01C10.17,8.81 13.83,8.81 16.09,11.01C16.25,10.85 17.56,9.57 17.73,9.41C14.57,6.33 9.44,6.33 6.27,9.41C6.6,9.73 7.75,10.85 7.91,11.01Z"
+        android:strokeAlpha="0" android:strokeColor="#000000" android:strokeWidth="1"/>
+    <path android:fillAlpha="1" android:fillColor="#ffffff" android:pathData="M14.8,18.11C14.79,17.93 14.73,16.47 14.72,16.29C14.71,16.07 14.83,15.87 15.02,15.79C15.62,15.5 16.19,15.11 16.67,14.62C16.89,14.41 17.26,14.42 17.5,14.66C17.65,14.81 18.86,16.02 19.01,16.17C19.25,16.41 19.26,16.78 19.05,17C15.34,20.7 9.05,20.42 4.99,16.36C4.75,16.12 4.74,15.75 4.95,15.53C5.09,15.39 6.2,14.29 6.33,14.15C6.55,13.93 6.92,13.95 7.16,14.19C7.7,14.73 8.3,15.17 8.93,15.51C9.12,15.61 9.26,15.81 9.27,16.05C9.28,16.17 9.3,16.77 9.35,17.87C11.82,18.36 13.64,18.44 14.8,18.11Z"/>
+    <path android:fillAlpha="0" android:fillColor="#FF000000"
+        android:pathData="M14.8,18.11C14.79,17.93 14.73,16.47 14.72,16.29C14.71,16.07 14.83,15.87 15.02,15.79C15.62,15.5 16.19,15.11 16.67,14.62C16.89,14.41 17.26,14.42 17.5,14.66C17.65,14.81 18.86,16.02 19.01,16.17C19.25,16.41 19.26,16.78 19.05,17C15.34,20.7 9.05,20.42 4.99,16.36C4.75,16.12 4.74,15.75 4.95,15.53C5.09,15.39 6.2,14.29 6.33,14.15C6.55,13.93 6.92,13.95 7.16,14.19C7.7,14.73 8.3,15.17 8.93,15.51C9.12,15.61 9.26,15.81 9.27,16.05C9.28,16.17 9.3,16.77 9.35,17.87C11.82,18.36 13.64,18.44 14.8,18.11Z"
+        android:strokeAlpha="0" android:strokeColor="#000000" android:strokeWidth="1"/>
+</vector>

packages/SystemUI/res/layout/operator_name.xml
@@ -24,9 +24,8 @@
         android:id="@+id/operator_name"
         android:layout_width="wrap_content"
         android:layout_height="match_parent"
-        android:maxLength="20"
+        android:maxLength="22"
         android:gravity="center_vertical|start"
         android:textAppearance="?android:attr/textAppearanceSmall"
-        android:singleLine="true"
-        android:paddingEnd="5dp" />
+        android:singleLine="true" />
 </com.android.systemui.statusbar.AlphaOptimizedFrameLayout>

commit 7141c124abe35d31bff7a9bdd9445a6bf49a9cee

fixup! Operator: Implement various UI requirements
Refractor after QPR2 merge.

Fixes: 53e9f4df4754 Operator: Implement various UI requirements
Issue: FP3-A13#109
Change-Id: Ibe8d4d3e2264baf021789246ca9aba0439fe862c

packages/SettingsLib/src/com/android/settingslib/mobile/TelephonyIcons.java
@@ -61,14 +61,6 @@ public class TelephonyIcons {
 
     public static final MobileIconGroup THREE_G_PLUS = new MobileIconGroup(
             "3G+",
-            null,
-            null,
-            AccessibilityContentDescriptions.PHONE_SIGNAL_STRENGTH,
-            0,
-            0,
-            0,
-            0,
-            AccessibilityContentDescriptions.PHONE_SIGNAL_STRENGTH[0],
             R.string.data_connection_3g,
             TelephonyIcons.ICON_3G_PLUS
     );