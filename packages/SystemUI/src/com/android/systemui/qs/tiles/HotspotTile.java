fairphone-mirror/platform_frameworks_base

commit a5b37b2c5b25a5235c6f329dc66e33b4896ad76a

Block access to Hotspot/Tethering when Airplane in ON
Block the access to Tethering option via Quick Settings
tile when either Airplane or Datasaver is turned ON.

Issue: PRJ8901-1237
Issue: FP3-A11#188
Issue: FP3-A11#233
Change-Id: I0ded76ed70b7acb35224ffc3426356cb4ebdc158

packages/SystemUI/src/com/android/systemui/qs/tiles/HotspotTile.java
@@ -21,6 +21,7 @@ import android.os.Handler;
 import android.os.Looper;
 import android.os.UserManager;
 import android.provider.Settings;
+import android.provider.Settings.Global;
 import android.service.quicksettings.Tile;
 import android.util.Log;
 import android.view.View;
@@ -38,11 +39,13 @@ import com.android.systemui.plugins.ActivityStarter;
 import com.android.systemui.plugins.FalsingManager;
 import com.android.systemui.plugins.qs.QSTile.BooleanState;
 import com.android.systemui.plugins.statusbar.StatusBarStateController;
+import com.android.systemui.qs.SettingObserver;
 import com.android.systemui.qs.QSHost;
 import com.android.systemui.qs.logging.QSLogger;
 import com.android.systemui.qs.tileimpl.QSTileImpl;
 import com.android.systemui.statusbar.policy.DataSaverController;
 import com.android.systemui.statusbar.policy.HotspotController;
+import com.android.systemui.util.settings.GlobalSettings;
 
 import javax.inject.Inject;
 
@@ -57,6 +60,8 @@ public class HotspotTile extends QSTileImpl<BooleanState> {
     private final HotspotAndDataSaverCallbacks mCallbacks = new HotspotAndDataSaverCallbacks();
     private boolean mListening;
 
+    private final SettingObserver mSetting;
+
     @Inject
     public HotspotTile(
             QSHost host,
@@ -68,7 +73,8 @@ public class HotspotTile extends QSTileImpl<BooleanState> {
             ActivityStarter activityStarter,
             QSLogger qsLogger,
             HotspotController hotspotController,
-            DataSaverController dataSaverController
+            DataSaverController dataSaverController,
+            GlobalSettings globalSettings
     ) {
         super(host, backgroundLooper, mainHandler, falsingManager, metricsLogger,
                 statusBarStateController, activityStarter, qsLogger);
@@ -76,6 +82,14 @@ public class HotspotTile extends QSTileImpl<BooleanState> {
         mDataSaverController = dataSaverController;
         mHotspotController.observe(this, mCallbacks);
         mDataSaverController.observe(this, mCallbacks);
+
+        mSetting = new SettingObserver(globalSettings, mHandler, Global.AIRPLANE_MODE_ON) {
+            @Override
+            protected void handleValueChanged(int value, boolean observedChange) {
+                // mHandler is the background handler so calling this is OK
+                handleRefreshState(value);
+            }
+        };
     }
 
     @Override
@@ -96,6 +110,7 @@ public class HotspotTile extends QSTileImpl<BooleanState> {
         if (listening) {
             refreshState();
         }
+        mSetting.setListening(listening);
     }
 
     @Override
@@ -111,7 +126,8 @@ public class HotspotTile extends QSTileImpl<BooleanState> {
     @Override
     protected void handleClick(@Nullable View view) {
         final boolean isEnabled = mState.value;
-        if (!isEnabled && mDataSaverController.isDataSaverEnabled()) {
+        if (mSetting.getValue() != 0
+                || (!isEnabled && mDataSaverController.isDataSaverEnabled())) {
             return;
         }
         // Immediately enter transient enabling state when turning hotspot on.
@@ -161,7 +177,8 @@ public class HotspotTile extends QSTileImpl<BooleanState> {
 
         final boolean isWifiTetheringAllowed =
                 WifiEnterpriseRestrictionUtils.isWifiTetheringAllowed(mHost.getUserContext());
-        final boolean isTileUnavailable = isDataSaverEnabled || !isWifiTetheringAllowed;
+        boolean isAirplaneMode = mSetting.getValue() != 0;
+        final boolean isTileUnavailable = isAirplaneMode || isDataSaverEnabled || !isWifiTetheringAllowed;
         final boolean isTileActive = (state.value || state.isTransient);
 
         if (isTileUnavailable) {

commit b3d1a2cd3179a7a92db146acac365871ff52266e

fixup! Block access to Hotspot/Tethering when Airplane in ON
Refractor after QPR2 merge.

Fixes: a5b37b2c5b25 Block access to Hotspot/Tethering when Airplane in ON
Issue: FP3-A13#109
Change-Id: I85d191e2d7f03585862b0f31777d2f14bdf81fcf

packages/SystemUI/src/com/android/systemui/qs/tiles/HotspotTile.java
@@ -83,7 +83,8 @@ public class HotspotTile extends QSTileImpl<BooleanState> {
         mHotspotController.observe(this, mCallbacks);
         mDataSaverController.observe(this, mCallbacks);
 
-        mSetting = new SettingObserver(globalSettings, mHandler, Global.AIRPLANE_MODE_ON) {
+        int currentUser = host.getUserContext().getUserId();
+        mSetting = new SettingObserver(globalSettings, mHandler, Global.AIRPLANE_MODE_ON, currentUser) {
             @Override
             protected void handleValueChanged(int value, boolean observedChange) {
                 // mHandler is the background handler so calling this is OK