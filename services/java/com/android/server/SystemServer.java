96099454bdf4ad9f099a8d9d9f6374d41572d99b
tianhai.sun <tianhai.sun@t2mobile.com>
2023-05-04 11:24:11 +0800

    [FP5-823] EN50332 Headphone safety test requires modifying headset volume prompt to damage hearing setting
    * Root Cause
      new function
    * Solution
      change usb safe media volume
    * Test Steps
      Press volume +
    
    Change-Id: I5ab26e8de344557638ee02a5a598899b59c8c063

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -1279,10 +1279,8 @@ public final class SystemServer implements Dumpable {
 
         mFirstBoot = mPackageManagerService.isFirstBoot();
         if (mFirstBoot){
-            Slog.i("sth__", "is_first_boot");
             SystemProperties.set("persist.sys.is_first_boot","1");
         } else {
-            Slog.i("sth__", "is_first_boot");
             SystemProperties.set("persist.sys.is_first_boot","0");
         }
         mPackageManager = mSystemContext.getPackageManager();
