6c873d17aec40607b4a4138acf1e2f6ea2236102
yubin.ying <yubin.ying@t2mobile.com>
2023-05-29 15:19:59 +0800

    [FP5-808] The navigation bar is unavailable when an application is opened
    * Root Cause
      system navigation bar cover the secondary
    * Solution
      set the system navigationbar invisible and not touchable
    * Test Steps
      connect to PC to test desktop navigation
    * Test Result
      pass
    
    Change-Id: Idbb473a0ec02871949e76f69dc94b5ca0aed03c9

--- packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBar.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBar.java
@@ -50,6 +50,8 @@ import static com.android.systemui.statusbar.phone.BarTransitions.TransitionMode
 import static com.android.systemui.statusbar.phone.CentralSurfaces.DEBUG_WINDOW_STATE;
 import static com.android.systemui.statusbar.phone.CentralSurfaces.dumpBarTransitions;
 import static com.android.systemui.util.Utils.isGesturalModeOnDefaultDisplay;
+import static android.provider.Settings.Global.DEVELOPMENT_FORCE_DESKTOP_MODE_ON_EXTERNAL_DISPLAYS;
+import static android.provider.Settings.Global.DEVELOPMENT_ENABLE_FREEFORM_WINDOWS_SUPPORT;
 
 import android.annotation.IdRes;
 import android.app.ActivityTaskManager;
@@ -95,6 +97,7 @@ import android.view.WindowManager;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityManager;
 import android.view.inputmethod.InputMethodManager;
+import android.provider.Settings;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
@@ -601,7 +604,7 @@ public class NavigationBar extends ViewController<NavigationBarView> implements 
         mView.updateRotationButton();
 
         mView.setVisibility(
-                mStatusBarKeyguardViewManager.isNavBarVisible() ? View.VISIBLE : View.INVISIBLE);
+                mStatusBarKeyguardViewManager.isNavBarVisible() && !isSecondaryDisplay() ? View.VISIBLE : View.INVISIBLE);
 
         if (DEBUG) Log.v(TAG, "addNavigationBar: about to add " + mView);
 
@@ -635,6 +638,17 @@ public class NavigationBar extends ViewController<NavigationBarView> implements 
 
         mNotificationShadeDepthController.addListener(mDepthListener);
     }
+
+    //ADD by T2M yingyubin for Desktop mode
+    private boolean isSecondaryDisplay(){
+        boolean desktopOn = Settings.Global.getInt(mContext.getContentResolver(),
+                DEVELOPMENT_FORCE_DESKTOP_MODE_ON_EXTERNAL_DISPLAYS,0) == 1
+                && Settings.Global.getInt(mContext.getContentResolver(),
+                DEVELOPMENT_ENABLE_FREEFORM_WINDOWS_SUPPORT, 0) == 1;
+        boolean isSecondaryDisplay = desktopOn && !mIsOnDefaultDisplay;
+        return isSecondaryDisplay;
+    }
+    //ADD by T2M yingyubin for Desktop mode
 
     public void destroyView() {
         setAutoHideController(/* autoHideController */ null);
@@ -1587,6 +1601,9 @@ public class NavigationBar extends ViewController<NavigationBarView> implements 
             lp.providedInsets = new InsetsFrameProvider[] {
                     new InsetsFrameProvider(ITYPE_NAVIGATION_BAR)
             };
+        }
+        if(isSecondaryDisplay()){
+            lp.flags |= WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE;
         }
         lp.token = new Binder();
         lp.accessibilityTitle = userContext.getString(R.string.nav_bar);
