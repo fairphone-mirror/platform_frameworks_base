4d43c10789ac6d5649874bf77582e4a0654c0404
renjiezhang <renjiezhang@t2mobile.com>
2023-06-09 16:36:42 +0800

    FP5-1698 [FP5 #122][Vodafone][CTC][QC#377] TTREP-6144 ( No user notification, also WLAN-Telefonie can be seen on Wifi calling option )
    * Root Cause
      AOSP default display
    * Solution
      customize vdf de display
    * Test Steps
      test with vdf de card,check Settings vowifi preference name
    
    Change-Id: Ib9696b3cba257a28b650b0350563a4420df0e324

--- telephony/java/android/telephony/CarrierConfigManager.java
+++ telephony/java/android/telephony/CarrierConfigManager.java
@@ -1851,7 +1851,9 @@ public class CarrierConfigManager {
      * <li>  9: WiFi Calling</li>
      * <li> 10: VoWifi</li>
      * <li> 11: %s WiFi Calling</li>
-     * <li> 12: WiFi Call</li>
+     * <li> 12: Vodafone WiFi Calling</li>
+     * <li> 13: %s  Wifi</li>
+     * <li> 14: WiFi Call</li>
      * @hide
      */
     public static final String KEY_WFC_SPN_FORMAT_IDX_INT = "wfc_spn_format_idx_int";
