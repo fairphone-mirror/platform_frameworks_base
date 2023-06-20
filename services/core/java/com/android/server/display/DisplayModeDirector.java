9c2db95036a88108a01eec6b634867bc98baa25f
haibiao.lu <haibiao.lu@t2mobile.com>
2023-06-20 13:43:35 +0800

    [FP5-1915]When the screen Refresh rate is set to 90hz, the blurred screen will appear in the boot animation
    * Root Cause
      drive is not ready when boot animation
    * Solution
      before bootcomplete use 60 flash rate
    * Test Steps
      Enter settings- display-set refresh rate 90Hz and reboot
    * Test Result
      pass
    
    Change-Id: I992eb98384db677d1e244baa9b5ac2617bdfabfc

--- services/core/java/com/android/server/display/DisplayModeDirector.java
+++ services/core/java/com/android/server/display/DisplayModeDirector.java
@@ -133,6 +133,7 @@ public class DisplayModeDirector {
     private DesiredDisplayModeSpecsListener mDesiredDisplayModeSpecsListener;
 
     private boolean mAlwaysRespectAppRequest;
+    private boolean mFirstUpdateFlashRate = false;
 
     /**
      * The allowed refresh rate switching type. This is used by SurfaceFlinger.
@@ -1211,8 +1212,13 @@ public class DisplayModeDirector {
 
         private void updateRefreshRateSettingLocked() {
             final ContentResolver cr = mContext.getContentResolver();
-            float minRefreshRate = Settings.System.getFloatForUser(cr,
+            float minRefreshRate = 60f;
+            if(mFirstUpdateFlashRate == false){
+                mFirstUpdateFlashRate = true;
+            }else {
+                minRefreshRate = Settings.System.getFloatForUser(cr,
                     Settings.System.MIN_REFRESH_RATE, 0f, cr.getUserId());
+            }
             float peakRefreshRate = Settings.System.getFloatForUser(cr,
                     Settings.System.PEAK_REFRESH_RATE, mDefaultPeakRefreshRate, cr.getUserId());
             updateRefreshRateSettingLocked(minRefreshRate, peakRefreshRate, mDefaultRefreshRate);
