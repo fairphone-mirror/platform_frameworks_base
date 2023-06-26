3d9356dcb8920f1a768e00a21affb66512e19e35
ruiliu <quic_ruiliu@quicinc.com>
2023-06-26 19:44:41 +0800

    SystemUI:Mofify register receiver as exported in MobileSignalController Added RECEIVER_EXPORTED flag to 4g customized broadcast.
    Change-Id: I2c6a4bd2d4c4e205a38a102938572058edcda68f
    CRs-Fixed: 3540824
    (cherry picked from commit f60cf82825ea1d35d8f32e6b86f1f2914296d536)

--- packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
@@ -219,9 +219,9 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
                 Global.MOBILE_DATA + mSubscriptionInfo.getSubscriptionId()),
                 true, mObserver);
         mContext.registerReceiver(mVolteSwitchObserver,
-                new IntentFilter("org.codeaurora.intent.action.ACTION_ENHANCE_4G_SWITCH"));
+                new IntentFilter("org.codeaurora.intent.action.ACTION_ENHANCE_4G_SWITCH"), Context.RECEIVER_EXPORTED);
         mContext.registerReceiver(
-                mVowifiChanged, new IntentFilter("arima.intent.action.VOWIFI_STATE_CHANGED"));
+                mVowifiChanged, new IntentFilter("arima.intent.action.VOWIFI_STATE_CHANGED"), Context.RECEIVER_NOT_EXPORTED);
     }
 
     /**
