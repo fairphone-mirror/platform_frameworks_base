77192662123449b13d452d9a04b6599ea3d5e93d
chuanzhi.shao <chuanzhi.shao@t2mobile.com>
2023-07-03 17:13:38 +0800

    [FP4S-1034] [FP4s-291] (Profilence) - FP4 A12 - BUILD SW1U/SP29 - android.os.RemoteException
    * Root Cause
      without unregisterReceiver
    * Solution
      add unregisterReceiver
    * Test Steps
      Monkey Test
    * Test Result
      pass
    
    Change-Id: I6a4065de467c1c5c1100c4af1652a9471f3d481c
    (cherry picked from commit 15f9acb856050b7ff95798b1e707366939d75aa9)

--- core/java/com/android/internal/util/ScreenshotHelper.java
+++ core/java/com/android/internal/util/ScreenshotHelper.java
@@ -420,4 +420,8 @@ public class ScreenshotHelper {
         mContext.sendBroadcastAsUser(errorIntent, UserHandle.CURRENT);
     }
 
+    public void unregisterReceiverByHelper(){
+        if(mContext != null)mContext.unregisterReceiver(mBroadcastReceiver);
+    }
+
 }
