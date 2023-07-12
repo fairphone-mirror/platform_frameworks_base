85f17f52bb68edbf8ad32494d54d8b70b9939061
haibiao.lu <haibiao.lu@t2mobile.com>
2023-07-12 16:31:03 +0800

    [FP5-1994] When manual ajust to 100%,the lowest brightness value should can be reduced to 0%
    * Root Cause
      lux is too bigger
    * Solution
      reset config when in low lux
    * Test Steps
      manual ajust to 100% enter dark enviroment
    * Test Result
      pass
    
    Change-Id: I30db89b1f4595375f01fe2c23d1a4b2591a54e67

--- services/core/java/com/android/server/display/AutomaticBrightnessController.java
+++ services/core/java/com/android/server/display/AutomaticBrightnessController.java
@@ -220,6 +220,8 @@ class AutomaticBrightnessController {
     private BrightnessThrottler mBrightnessThrottler;
     private boolean mIsBrightnessThrottled;
 
+    private int mLowluxlevelTimes = 0;
+
     // Context-sensitive brightness configurations require keeping track of the foreground app's
     // package name and category, which is done by registering a TaskStackListener to call back to
     // us onTaskStackChanged, and then using the ActivityTaskManager to get the foreground app's
@@ -633,6 +635,7 @@ class AutomaticBrightnessController {
             mRecentLightSamples = 0;
             mAmbientLightRingBuffer.clear();
             mCurrentLightSensorRate = -1;
+            mLowluxlevelTimes = 0;
             mHandler.removeMessages(MSG_UPDATE_AMBIENT_LUX);
             unregisterForegroundAppUpdater();
             mSensorManager.unregisterListener(mLightSensorListener);
@@ -1157,6 +1160,15 @@ class AutomaticBrightnessController {
         public void onSensorChanged(SensorEvent event) {
             if (mLightSensorEnabled) {
                 final long time = mClock.uptimeMillis();
+                if(event.values[0] < 100f){
+                    mLowluxlevelTimes++;
+                }else{
+                    mLowluxlevelTimes = 0;
+                }
+                if (mLowluxlevelTimes == 45 && mShortTermModelAnchor != -1){
+                    resetShortTermModel();
+                    updateAutoBrightness(true, false);
+                }
                 if(event.values[0] < 1.0f){
                     handleLightSensorEvent(time, 0f);
                 }else {
