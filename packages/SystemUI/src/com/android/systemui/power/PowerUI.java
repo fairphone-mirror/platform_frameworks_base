b99a657cf0d96608bf6d47310a52e2d9b986e5e3
tianhai.sun <tianhai.sun@t2mobile.com>
2023-07-20 13:55:41 +0800

    [FP5-2245] [FP5]Remove useless log
    Change-Id: I9d3688a908665c28fe000bffeaca0bfa61c29847

--- packages/SystemUI/src/com/android/systemui/power/PowerUI.java
+++ packages/SystemUI/src/com/android/systemui/power/PowerUI.java
@@ -188,10 +188,7 @@ public class PowerUI extends CoreStartable implements CommandQueue.Callbacks {
                     public void onChange(boolean selfChange) {
                         if ("isBoot".equals(Settings.Global.getString(mContext.getContentResolver(),
                                 Settings.Global.SET_BATTERY_CHARGING_MODE))){
-                            Log.i(TAG,"------ isBoot");
                         }else {
-                            Log.i(TAG,"------ SET_BATTERY_CHARGING_MODE :" + Settings.Global.getString(mContext.getContentResolver(),
-                                    Settings.Global.SET_BATTERY_CHARGING_MODE));
                             setBatteryChargingMode();
                         }
                     }
@@ -524,7 +521,6 @@ public class PowerUI extends CoreStartable implements CommandQueue.Callbacks {
                 int batteryHealth = intent.getIntExtra(Intent.EXTRA_BATTERY_HEALTH, 0);
                 int batteryTemperature = intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, 0);
                 int batteryStatus = intent.getIntExtra(BatteryManager.EXTRA_STATUS, BatteryManager.BATTERY_STATUS_UNKNOWN);
-                Log.i(TAG, "receive ACTION_BATTERY_WARM_TEMP_CHANGED:batteryTemperature:"+batteryTemperature+"  batteryStatus:"+batteryStatus);
                 if (batteryHealth == BatteryManager.BATTERY_HEALTH_OVERHEAT) {
                     mWarnings.showHighTemp(true,batteryStatus,batteryTemperature,batteryHealth);
                 } else if (batteryHealth == BatteryManager.BATTERY_HEALTH_COLD) {
