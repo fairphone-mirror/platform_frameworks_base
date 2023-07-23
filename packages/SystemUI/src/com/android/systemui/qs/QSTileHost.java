bcb543771c01522cbb3b741c67cff9956a5e86e0
xiujuan.yuan <xiujuan.yuan@t2mobile.com>
2023-07-23 14:55:16 +0800

    [FP5-2230][FP5 #263][Vodafone][CTC][QC#402] [GoogleRSA] Device controls and Wallet are missing in the 1st page of the quick settings * Root Cause CR * Solution * Test Step insert VF white cards ,factory reset ,setupwizard,install wallet ,Google Home
    Change-Id: If633f38c0494fdf11c3827034eac67c7ecfa80c2

--- packages/SystemUI/src/com/android/systemui/qs/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSTileHost.java
@@ -335,6 +335,7 @@ public class QSTileHost implements QSHost, Tunable, PluginListener<QSFactory>, D
                     tile.getValue().destroy();
                 });
         final LinkedHashMap<String, QSTile> newTiles = new LinkedHashMap<>();
+        boolean isCreateControllers = false;
         for (String tileSpec : tileSpecs) {
             QSTile tile = mTiles.get(tileSpec);
             if (tile != null && (!(tile instanceof CustomTile)
@@ -367,6 +368,7 @@ public class QSTileHost implements QSHost, Tunable, PluginListener<QSFactory>, D
                         tile.setTileSpec(tileSpec);
                         if (tile.isAvailable()) {
                             newTiles.put(tileSpec, tile);
+                            isCreateControllers = tileSpec.equals("controls");
                             mQSLogger.logTileAdded(tileSpec);
                         } else {
                             tile.destroy();
@@ -383,6 +385,12 @@ public class QSTileHost implements QSHost, Tunable, PluginListener<QSFactory>, D
         List<String> currentSpecs = new ArrayList<>(mTileSpecs);
         mTileSpecs.clear();
         mTileSpecs.addAll(newTiles.keySet()); // Only add the valid (available) tiles.
+
+        if(isCreateControllers && mTileSpecs.contains("controls")){
+            mTileSpecs.remove("controls");
+            mTileSpecs.add(4,"controls");
+        }
+
         mTiles.clear();
         mTiles.putAll(newTiles);
         if (newTiles.isEmpty() && !tileSpecs.isEmpty()) {
