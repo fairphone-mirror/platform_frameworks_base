d4faef81f88e9b33298a0477ad3ddbe7b6006cfb
xiangyu.deng <xiangyu.deng@t2mobile.com>
2023-08-04 16:51:38 +0800

    [FP4T-594] Confusion icon appear in top of the screen
    * Root Cause
      show signal strength ICON afer IMS register
    * Solution
      disable signal strength ICON
    * Test Steps
      check Dropdown user interface
    * Test Result
      pass
    
    Change-Id: If4cd152e24e13b66da94557ee77ab8e5b5a0bf3c

--- packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
@@ -270,7 +270,9 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
         };
         mImsMmTelManager = ImsMmTelManager.createForSubscriptionId(info.getSubscriptionId());
         mMobileStatusTracker = mobileStatusTrackerFactory.createTracker(mMobileCallback);
-        mProviderModelBehavior = featureFlags.isEnabled(Flags.COMBINED_STATUS_BAR_SIGNAL_ICONS);
+        // modify for FP4T-594 by T2M.dengxiangyu 2023-08-04
+        //mProviderModelBehavior = featureFlags.isEnabled(Flags.COMBINED_STATUS_BAR_SIGNAL_ICONS);
+        mProviderModelBehavior = false;
     }
 
     void setConfiguration(Config config) {
