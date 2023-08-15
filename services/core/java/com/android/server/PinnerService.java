8560ca51004bc7170a5900552c3ea7cf9581ff04
haibiao.lu <haibiao.lu@t2mobile.com>
2023-08-15 17:45:39 +0800

    [FP5-2531] add workround to fix the default value for screen timeout is null
    * Root Cause
      factory will run adb shell pm clear com.google.android.setupwizard
    * Solution
      add workround
    * Test Steps
      follow factory method
    * Test Result
      pass
    
    Change-Id: Icece8ef1905e4f832798562460f5d911eaedc7a1

--- services/core/java/com/android/server/PinnerService.java
+++ services/core/java/com/android/server/PinnerService.java
@@ -85,6 +85,7 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
 import android.content.ComponentName;
+import static android.provider.Settings.System.SCREEN_OFF_TIMEOUT;
 
 /**
  * <p>PinnerService pins important files for key processes in memory.</p>
@@ -368,6 +369,9 @@ public final class PinnerService extends SystemService {
                         if (userSetupCompleteUri.equals(uri)) {
                             sendPinAppMessage(KEY_HOME, ActivityManager.getCurrentUser(),
                                     true /* force */);
+                            if (isUserSetupCompleted() && isMyPhoneFirstOpen()){
+                                 Settings.System.putInt(mContext.getContentResolver(), SCREEN_OFF_TIMEOUT, 60*1000); 
+                            }
                             if (isUserSetupCompleted() && isMyPhoneFirstOpen() && !isDtCarrier()) {
                                 mPinnerHandler.postDelayed(new Runnable(){
                                     @Override
