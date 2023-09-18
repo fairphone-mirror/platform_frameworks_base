dc5684ba4b9f0a0af51fb183970ecd34f19798f8
haibiao.lu <haibiao.lu@t2mobile.com>
2023-09-18 14:17:14 +0800

    [FP4T-716] Adaptive brightness remembers last set brightness level
    * Root Cause
      reset config when switch Adaptive brightness
    * Solution
      reset config when switch Adaptive brightness
    * Test Steps
      User disable adaptive brightness,changes brightness level manually than enable adaptive
    * Test Result
      pass
    
    Change-Id: I2e75c7ffd66a75d1a3e8c81aed15adeed8c3a225
    (cherry picked from commit d7cbd4978aaa1eab230610b291d2c4e85078f96a)

--- services/core/java/com/android/server/display/DisplayPowerController.java
+++ services/core/java/com/android/server/display/DisplayPowerController.java
@@ -491,6 +491,8 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
     private final String mSuspendBlockerIdProxNegative;
     private final String mSuspendBlockerIdProxDebounce;
 
+    private String old_screen_mode = "0";
+
     /**
      * Creates the display power controller.
      */
@@ -935,6 +937,10 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
         mContext.getContentResolver().registerContentObserver(
                 Settings.System.getUriFor(Settings.System.SCREEN_AUTO_BRIGHTNESS_ADJ),
                 false /*notifyForDescendants*/, mSettingsObserver, UserHandle.USER_ALL);
+        mContext.getContentResolver().registerContentObserver(
+                Settings.System.getUriFor(Settings.System.SCREEN_BRIGHTNESS_MODE),
+                false /*notifyForDescendants*/, mSettingsObserver, UserHandle.USER_ALL);
+
     }
 
     private void setUpAutoBrightness(Resources resources, Handler handler) {
@@ -3075,7 +3081,17 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
 
         @Override
         public void onChange(boolean selfChange, Uri uri) {
-            handleSettingsChange(false /* userSwitch */);
+            String screen_mode = Settings.System.getString(mContext.getContentResolver(),
+                    Settings.System.SCREEN_BRIGHTNESS_MODE);
+            if ("0".equals(screen_mode)){
+                old_screen_mode = screen_mode;
+            }
+            if ("1".equals(screen_mode) && "0".equals(old_screen_mode)){
+                old_screen_mode = screen_mode;
+                handleSettingsChange(true );
+            }else {
+                handleSettingsChange(false /* userSwitch */);
+            }
         }
     }
 
