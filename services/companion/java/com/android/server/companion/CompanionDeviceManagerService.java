dea7b1c5ada22c28019be76d6b91d09af05e9a32
Raphael Kim <raphk@google.com>
2023-09-18 14:07:23 -0700

    Validate component name length before requesting notification access.
    Bug: 295335110
    Test: Test app with long component name
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:618767b6bf71b872399b57eb89c6724041258f45)
    Merged-In: I7ea5d5c1f78858db9865f3310d1e0aff9c8b5579
    Change-Id: I7ea5d5c1f78858db9865f3310d1e0aff9c8b5579
    (cherry picked from commit 900fe7d8a43550160800cb6c05d408969f801c23)
    (cherry picked from commit 43424651dd8ce56a5a07b42ec0d5cfad21d8fa93)

--- services/companion/java/com/android/server/companion/CompanionDeviceManagerService.java
+++ services/companion/java/com/android/server/companion/CompanionDeviceManagerService.java
@@ -132,6 +132,7 @@ public class CompanionDeviceManagerService extends SystemService {
             "debug.cdm.cdmservice.removal_time_window";
 
     private static final long ASSOCIATION_REMOVAL_TIME_WINDOW_DEFAULT = DAYS.toMillis(90);
+    private static final int MAX_CN_LENGTH = 500;
 
     private final ActivityManager mActivityManager;
     private final OnPackageVisibilityChangeListener mOnPackageVisibilityChangeListener;
@@ -620,6 +621,9 @@ public class CompanionDeviceManagerService extends SystemService {
                 throws RemoteException {
             String callingPackage = component.getPackageName();
             checkCanCallNotificationApi(callingPackage, userId);
+            if (component.flattenToString().length() > MAX_CN_LENGTH) {
+                throw new IllegalArgumentException("Component name is too long.");
+            }
             final long identity = Binder.clearCallingIdentity();
             try {
                 return PendingIntent.getActivityAsUser(getContext(),
