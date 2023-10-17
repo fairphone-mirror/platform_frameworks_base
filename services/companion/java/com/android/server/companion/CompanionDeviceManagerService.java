928aa4dfc71617c3258652d5a442deb0b08cfc34
Evan Chen <evanxinchen@google.com>
2023-10-17 21:31:23 +0000

    Do not allow setting notification access across users.
    For mutil user case, make sure the calling userid
    matching the passing userid
    
    Test: test it on sample app
    Bug: 298635078
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:12bb4ed9ab46d3e42326ef1c5e7b90aae80a9bfc)
    Merged-In: I6c478ebcc1d981faf2d125a4b41909c3b6a30a2a
    Change-Id: I6c478ebcc1d981faf2d125a4b41909c3b6a30a2a

--- services/companion/java/com/android/server/companion/CompanionDeviceManagerService.java
+++ services/companion/java/com/android/server/companion/CompanionDeviceManagerService.java
@@ -684,8 +684,7 @@ public class CompanionDeviceManagerService extends SystemService {
         public PendingIntent requestNotificationAccess(ComponentName component, int userId)
                 throws RemoteException {
             String callingPackage = component.getPackageName();
-            checkCanCallNotificationApi(callingPackage);
-            // TODO: check userId.
+            checkCanCallNotificationApi(callingPackage, userId);
             final long identity = Binder.clearCallingIdentity();
             try {
                 return PendingIntent.getActivityAsUser(getContext(),
@@ -708,7 +707,7 @@ public class CompanionDeviceManagerService extends SystemService {
         @Deprecated
         @Override
         public boolean hasNotificationAccess(ComponentName component) throws RemoteException {
-            checkCanCallNotificationApi(component.getPackageName());
+            checkCanCallNotificationApi(component.getPackageName(), getCallingUserId());
             NotificationManager nm = getContext().getSystemService(NotificationManager.class);
             return nm.isNotificationListenerAccessGranted(component);
         }
@@ -904,8 +903,7 @@ public class CompanionDeviceManagerService extends SystemService {
             createNewAssociation(userId, packageName, macAddressObj, null, null, false);
         }
 
-        private void checkCanCallNotificationApi(String callingPackage) {
-            final int userId = getCallingUserId();
+        private void checkCanCallNotificationApi(String callingPackage, int userId) {
             enforceCallerIsSystemOr(userId, callingPackage);
 
             if (getCallingUid() == SYSTEM_UID) return;
