60e23a9455526bb71bc91b1d56d2a63f8e79a9d9
haibiao.lu <haibiao.lu@t2mobile.com>
2024-01-10 15:06:53 +0800

    [FP5U-105] [Photos]Open screenshot in the bottom left corner and prompt Editing not supported, screenshot can not be open
    * Root Cause
      uri contains user id,photos can not support this uri
    * Solution
      do not add userid to uri
    * Test Steps
      [PASS] screen shot and edit
    * Bug Source Type
      Development
    
    Change-Id: If58a40f88e90d6cda9bdee816b0b971e378c7ed7

--- packages/SystemUI/src/com/android/systemui/screenshot/ImageExporter.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/ImageExporter.java
@@ -295,7 +295,9 @@ public class ImageExporter {
             Uri baseUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
             Uri uriWithUserId = ContentProvider.maybeAddUserId(baseUri, owner.getIdentifier());
 
-            Uri uri = resolver.insert(uriWithUserId, values);
+            //Modify by luhaibiao for fix [FP5U-105] screen shot can not be edit 
+            //Uri uri = resolver.insert(uriWithUserId, values);
+            Uri uri = resolver.insert(baseUri, values);
             if (uri == null) {
                 throw new ImageExportException(RESOLVER_INSERT_RETURNED_NULL);
             }
