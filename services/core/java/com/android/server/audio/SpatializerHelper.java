d33c4c8d19d1115776b10d13f03036b31a003e65
Prashantsinh <prashantsinh.parmar@fairphone.partners>
2024-05-16 18:39:24 +0530

    Reapply "Spatializer: Fix lost settings after reboot"
    This reverts commit e267aa0eaf6eac7f42faa769187b394aceddd635.
    
    Issue: FP3-A13#298
    Change-Id: I33df9d6d9e5f09885d539ac64007937c9d34eccb

--- services/core/java/com/android/server/audio/SpatializerHelper.java
+++ services/core/java/com/android/server/audio/SpatializerHelper.java
@@ -180,7 +180,7 @@ public class SpatializerHelper {
         SADeviceState.sHeadTrackingEnabledDefault = headTrackingEnabledByDefault;
     }
 
-    synchronized void init(boolean effectExpected) {
+    synchronized void init(boolean effectExpected, @Nullable String settings) {
         loglogi("init effectExpected=" + effectExpected);
         if (!effectExpected) {
             loglogi("init(): setting state to STATE_NOT_SUPPORTED due to effect not expected");
@@ -283,6 +283,13 @@ public class SpatializerHelper {
                     mSACapableDeviceTypes.add(SPAT_MODE_FOR_DEVICE_TYPE.keyAt(i));
                 }
             }
+
+            // When initialized from AudioService, the settings string will be non-null.
+            // Saved settings need to be applied after spatialization support is initialized above.
+            if (settings != null) {
+                setSADeviceSettings(settings);
+            }
+
             // for both transaural / binaural, we are not forcing enablement as the init() method
             // could have been called another time after boot in case of audioserver restart
             addCompatibleAudioDevice(
@@ -320,7 +327,7 @@ public class SpatializerHelper {
         mState = STATE_UNINITIALIZED;
         mSpatLevel = Spatializer.SPATIALIZER_IMMERSIVE_LEVEL_NONE;
         mActualHeadTrackingMode = Spatializer.HEAD_TRACKING_MODE_UNSUPPORTED;
-        init(true);
+        init(true, null /* settings */);
         setSpatializerEnabledInt(featureEnabled);
     }
 
@@ -757,7 +764,7 @@ public class SpatializerHelper {
                 return;
             }
             if (mState == STATE_UNINITIALIZED) {
-                init(true);
+                init(true, null /* settings */);
             }
             setSpatializerEnabledInt(true);
         } else {
