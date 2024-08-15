3c0c3952186db2c5249a025e946f7c338553cf09
xiaoyang.wang <xiaoyang.wang@t2mobile.com>
2024-08-15 10:10:49 +0800

    FP5U-554 [FP5U #617]Accessibility menu shortcut disappearing[1/2]
    * Root Cause
      Requirement
    * Solution
      Solving problems caused by DIM
    * Test Steps
      1.Enable the Accessibility menu shortcut  2.Go to FP5 Display settings  3.Shortcut is disabled
    
    Change-Id: I462f68dfba56ee07459f4140d128bb495c2d6738
    (cherry picked from commit 48a2974a51fed89f01414592ebb222b2c2371739)

--- packages/SystemUI/src/com/android/systemui/qs/tiles/ReduceBrightColorsTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/ReduceBrightColorsTile.java
@@ -67,6 +67,7 @@ public class ReduceBrightColorsTile extends QSTileImpl<QSTile.BooleanState>
     private int mLageLuxCounter = 0 ;
     private final float CAN_ENTRY_EXTRA_DIM_VALUE = 80;
     private boolean mRegistLightsensor = false;
+    private final String ACCESSIBILITY_BUTTON_TARGETS_STRING = "com.android.server.accessibility/ReduceBrightColors";
 
     @Inject
     public ReduceBrightColorsTile(
@@ -137,7 +138,7 @@ public class ReduceBrightColorsTile extends QSTileImpl<QSTile.BooleanState>
                 mReduceBrightColorsController.setReduceBrightColorsActivated(false);
                 Secure.putInt(mContext.getContentResolver(),Secure.ENABLE_REDUCE_BRIGHT_COLORS,0);
                 Secure.putString(mContext.getContentResolver(),Secure.ACCESSIBILITY_SHORTCUT_TARGET_SERVICE,"");
-                Secure.putString(mContext.getContentResolver(),Secure.ACCESSIBILITY_BUTTON_TARGETS,"");
+                Secure.putString(mContext.getContentResolver(),Secure.ACCESSIBILITY_BUTTON_TARGETS,getButtonTargetsString());
             }
             if(mSmallLuxCounter == 10){
                 Secure.putInt(mContext.getContentResolver(),Secure.ENABLE_REDUCE_BRIGHT_COLORS,1);
@@ -149,6 +150,23 @@ public class ReduceBrightColorsTile extends QSTileImpl<QSTile.BooleanState>
             // Not used.
         }
     };
+
+    private String getButtonTargetsString() {
+        String mCurrentAccessibilityButtonTargets = Settings.Secure.getString(mContext.getContentResolver(),Secure.ACCESSIBILITY_BUTTON_TARGETS);
+        String[] strings = mCurrentAccessibilityButtonTargets.split(":");
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < strings.length; i++) {
+            if (!ACCESSIBILITY_BUTTON_TARGETS_STRING.equals(strings[i])){
+                if (i == 0) {
+                    result.append(strings[i]);
+                } else {
+                    result.append(":");
+                    result.append(strings[i]);
+                }
+            }
+        }
+        return result.toString();
+    }
 
     @Override
     protected void handleDestroy() {
