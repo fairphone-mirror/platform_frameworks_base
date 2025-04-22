OrionOS-Project/frameworks_base

Fix: Block opening settings app on keyguard without user auth
Test: atest KeyGestureEventTests
Bug: 378900798
Flag: EXEMPT bugfix
(cherry picked from commit 6019c6b715aad9f6bd4c296c74b63e2ef406ad6c)
(cherry picked from https://googleplex-android-review.googlesource.com/q/commit:e4d483a2ef99a71c6fcd6ad2e6c2f8f88ba380f4)
Merged-In: I89d43872108710d20e0c4ef7e652d389896155d7
Change-Id: I89d43872108710d20e0c4ef7e652d389896155d7

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -3110,7 +3110,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 }
                 break;
             case KeyEvent.KEYCODE_I:
-                if (firstDown && event.isMetaPressed()) {
+                if (firstDown && event.isMetaPressed() && isUserSetupComplete() && !keyguardOn) {
                     showSystemSettings();
                     logKeyboardSystemsEvent(event, KeyboardLogEvent.LAUNCH_SYSTEM_SETTINGS);
                     return true;

fairphone-mirror/platform_frameworks_base

(cherry picked from commit d615298466085c4a88c6733804160e0c1ee7e31e)
(cherry picked from https://googleplex-android-review.googlesource.com/q/commit:e832d2b23784ede62259fb7518573e12dced2209)
(cherry picked from commit c1b79f65fc190e9a8e7f90e60d9609357c2ba6b1)

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -3107,7 +3107,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 }
                 break;
             case KeyEvent.KEYCODE_I:
-                if (down && event.isMetaPressed()) {
+                if (down && event.isMetaPressed() && isUserSetupComplete() && !keyguardOn) {
                     showSystemSettings();
                     return key_consumed;
                 }