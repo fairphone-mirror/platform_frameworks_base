ProjectNirwana/frameworks_base

Do not allow to set device owner while accounts present
Bug: 417988098
Test: manual
Flag: EXEMPT BUGFIX
Change-Id: I51c7253b124ab524b817b17dc0aabbdb5ab12ac7
Merged-In: I51c7253b124ab524b817b17dc0aabbdb5ab12ac7

services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
@@ -364,6 +364,7 @@ import com.android.internal.widget.LockSettingsInternal;
 import com.android.internal.widget.LockscreenCredential;
 import com.android.internal.widget.PasswordValidationError;
 import com.android.net.module.util.ProxyUtils;
+import com.android.server.accounts.AccountManagerService;
 import com.android.server.LocalServices;
 import com.android.server.LockGuard;
 import com.android.server.PersistentDataBlockManagerInternal;
@@ -8424,6 +8425,17 @@ public class DevicePolicyManagerService extends BaseIDevicePolicyManager {
         // Cannot be called while holding the lock:
         final boolean hasIncompatibleAccountsOrNonAdb =
                 hasIncompatibleAccountsOrNonAdbNoLock(caller, userId, admin);
+
+        if (!hasIncompatibleAccountsOrNonAdb) {
+            synchronized (getLockObject()) {
+                if (!isAdminTestOnlyLocked(admin, userId) && hasAccountsOnAnyUser()) {
+                    Slogf.w(LOG_TAG,
+                            "Non test-only owner can't be installed with existing accounts.");
+                    return false;
+                }
+            }
+        }
+
         synchronized (getLockObject()) {
             enforceCanSetDeviceOwnerLocked(caller, admin, userId, hasIncompatibleAccountsOrNonAdb);
             Preconditions.checkArgument(isPackageInstalledForUser(admin.getPackageName(), userId),
@@ -18808,6 +18820,11 @@ public class DevicePolicyManagerService extends BaseIDevicePolicyManager {
         AccountManager am = AccountManager.get(mContext);
         Account[] accounts = am.getAccounts();
         return accounts.length == 0;
+    }
+
+    private boolean hasAccountsOnAnyUser() {
+        AccountManagerService accountManagerService = AccountManagerService.getSingleton();
+        return accountManagerService.getAllAccounts().length != 0;
     }
 
     private void setBypassDevicePolicyManagementRoleQualificationStateInternal(

fairphone-mirror/platform_frameworks_base

services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
@@ -364,6 +364,7 @@ import com.android.internal.widget.LockSettingsInternal;
 import com.android.internal.widget.LockscreenCredential;
 import com.android.internal.widget.PasswordValidationError;
 import com.android.net.module.util.ProxyUtils;
+import com.android.server.accounts.AccountManagerService;
 import com.android.server.LocalServices;
 import com.android.server.LockGuard;
 import com.android.server.PersistentDataBlockManagerInternal;
@@ -8428,6 +8429,17 @@ public class DevicePolicyManagerService extends BaseIDevicePolicyManager {
         // Cannot be called while holding the lock:
         final boolean hasIncompatibleAccountsOrNonAdb =
                 hasIncompatibleAccountsOrNonAdbNoLock(caller, userId, admin);
+
+        if (!hasIncompatibleAccountsOrNonAdb) {
+            synchronized (getLockObject()) {
+                if (!isAdminTestOnlyLocked(admin, userId) && hasAccountsOnAnyUser()) {
+                    Slogf.w(LOG_TAG,
+                            "Non test-only owner can't be installed with existing accounts.");
+                    return false;
+                }
+            }
+        }
+
         synchronized (getLockObject()) {
             enforceCanSetDeviceOwnerLocked(caller, admin, userId, hasIncompatibleAccountsOrNonAdb);
             Preconditions.checkArgument(isPackageInstalledForUser(admin.getPackageName(), userId),
@@ -18818,6 +18830,11 @@ public class DevicePolicyManagerService extends BaseIDevicePolicyManager {
         AccountManager am = AccountManager.get(mContext);
         Account[] accounts = am.getAccounts();
         return accounts.length == 0;
+    }
+
+    private boolean hasAccountsOnAnyUser() {
+        AccountManagerService accountManagerService = AccountManagerService.getSingleton();
+        return  accountManagerService.getAllAccountsForSystemProcess().length != 0;
     }
 
     private void setBypassDevicePolicyManagementRoleQualificationStateInternal(